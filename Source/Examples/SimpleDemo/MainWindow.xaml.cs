a87bbf207e7d622157250d7b7e60678eae847734
objo <objo@localhost>
2012-04-20 22:35:46 +0200

    Refactoring of Examples - moved most examples into a common application "ExampleBrowser". Included NAudio as a nuget package. Changed the SimpleDemo to be really simple.

---
+++ Source/Examples/SimpleDemo/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ExampleBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
