6f0cb1182a7919bc7a55eeb6f4bd9642bdb01d12
objo <objo@localhost>
2012-09-05 22:39:39 +0200

    Added .NET 4.5 build, upgraded example projects to .NET 4.5

--- Tools/pack.cmd
+++ Tools/pack.cmd
@@ -1,6 +1,11 @@
 mkdir ..\Packages\HelixToolkit\lib
-copy ..\Output\HelixToolkit.Wpf.dll ..\Packages\HelixToolkit\lib
-copy ..\Output\HelixToolkit.Wpf.xml ..\Packages\HelixToolkit\lib
-copy ..\Output\HelixToolkit.Wpf.pdb ..\Packages\HelixToolkit\lib
+mkdir ..\Packages\HelixToolkit\lib\NET40
+mkdir ..\Packages\HelixToolkit\lib\NET45
+copy ..\Output\NET40\HelixToolkit.Wpf.dll ..\Packages\HelixToolkit\lib\NET40
+copy ..\Output\NET40\HelixToolkit.Wpf.xml ..\Packages\HelixToolkit\lib\NET40
+copy ..\Output\NET40\HelixToolkit.Wpf.pdb ..\Packages\HelixToolkit\lib\NET40
+copy ..\Output\NET45\HelixToolkit.Wpf.dll ..\Packages\HelixToolkit\lib\NET45
+copy ..\Output\NET45\HelixToolkit.Wpf.xml ..\Packages\HelixToolkit\lib\NET45
+copy ..\Output\NET45\HelixToolkit.Wpf.pdb ..\Packages\HelixToolkit\lib\NET45
 copy ..\license.txt ..\Packages\HelixToolkit
 nuget.exe pack ..\Packages\HelixToolkit\HelixToolkit.nuspec -OutputDirectory ..\Packages > pack.log
