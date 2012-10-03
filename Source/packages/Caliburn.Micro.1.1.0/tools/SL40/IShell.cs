05f1535f8db1ef9490bc21325b37ad2fa66f32a3
objo <objo@localhost>
2012-10-03 20:09:40 +0200

    Removed lib\ folders from new example projects. Added PepitaGet and added a prebuild step in the projects that use NuGet packages Removed nuget packages from the packages\ folder Added Resharper 7.0 settings file (team shared on solution)   (default settings + StyleCop + no regions)

--- Source/packages/Caliburn.Micro.1.1.0/tools/SL40/IShell.cs
+++
@@ -1,9 +0,0 @@
-// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="IShell.cs" company="Helix 3D Toolkit">
-//   http://helixtoolkit.codeplex.com, license: Ms-PL
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
-namespace $safeprojectname$ {
-    public interface IShell {}
-}
