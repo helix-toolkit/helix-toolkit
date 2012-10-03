7d9edb1df17a97caeb1a74431ad4bc7c242ec756
dturvey_bhc <dturvey_bhc@localhost>
2012-10-03 14:40:46 +0100

    Added Demo projects for bindable manipulators and for camera animation bug.

---
+++ Source/Examples/MvvmCameraDemo/ViewModels/ViewportViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Media3D;
+using Caliburn.Micro;
+using System.Diagnostics;
+
+namespace MvvmCameraDemo.ViewModels
+{
+    public class ViewportViewModel : Screen
+    {
+        PerspectiveCamera _camera;
+        public PerspectiveCamera Camera
+        {
+            get { return _camera; }
+            set
+            {
+                _camera = value;
+                NotifyOfPropertyChange(() => this.Camera);
+            }
+        }
+
+
+        public ViewportViewModel()
+        {
+
+        }
+    }
+}
