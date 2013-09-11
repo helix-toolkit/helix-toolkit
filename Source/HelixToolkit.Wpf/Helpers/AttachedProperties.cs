8fb4bcef4ff74818d8156ac18b9ab84ae8b3c29a
Oystein Bjorke <oystein.bjorke@gmail.com>
2013-09-11 20:38:39 +0200

    Changed attached property "Name" owner type to DependencyObject. Added Name to Materials in obj importer.

--- Source/HelixToolkit.Wpf/Helpers/AttachedProperties.cs
+++ Source/HelixToolkit.Wpf/Helpers/AttachedProperties.cs
@@ -1,21 +1,20 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Visual3DProperties.cs" company="Helix 3D Toolkit">
+// <copyright file="AttachedProperties.cs" company="Helix 3D Toolkit">
 //   http://helixtoolkit.codeplex.com, license: MIT
 // </copyright>
 // <summary>
-//   A control that contains a <see cref="Viewport3D" /> and a <see cref="CameraController" /> .
+//   Provides attached properties.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
 namespace HelixToolkit.Wpf
 {
     using System.Windows;
-    using System.Windows.Media.Media3D;
 
     /// <summary>
-    /// Provides attached properties for <see cref="Visual3D"/>.
+    /// Provides attached properties.
     /// </summary>
-    public static class Visual3DProperties
+    public static class AttachedProperties
     {
         /// <summary>
         /// The name property
@@ -23,25 +22,25 @@ namespace HelixToolkit.Wpf
         public static readonly DependencyProperty NameProperty = DependencyProperty.RegisterAttached(
             "Name",
             typeof(string),
-            typeof(Visual3D),
+            typeof(DependencyObject),
             new PropertyMetadata(null));
 
         /// <summary>
-        /// Gets the name of the <see cref="Visual3D"/>.
+        /// Gets the name of the model.
         /// </summary>
-        /// <param name="obj">The visual.</param>
+        /// <param name="obj">The model.</param>
         /// <returns>The name.</returns>
-        public static string GetName(this Visual3D obj)
+        public static string GetName(this DependencyObject obj)
         {
             return (string)obj.GetValue(NameProperty);
         }
 
         /// <summary>
-        /// Sets the name of the <see cref="Visual3D"/>.
+        /// Sets the name of the model.
         /// </summary>
-        /// <param name="obj">The visual.</param>
-        /// <param name="value">The name.</param>
-        public static void SetName(this Visual3D obj, string value)
+        /// <param name="obj">The model.</param>
+        /// <param name="value">The value.</param>
+        public static void SetName(this DependencyObject obj, string value)
         {
             obj.SetValue(NameProperty, value);
         }
