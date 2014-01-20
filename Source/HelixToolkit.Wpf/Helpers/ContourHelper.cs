839c8dcc120c52d5285cc6f52e6ac122b5c21c1d
christophano <christophano@hotmail.com>
2014-01-20 13:18:14 +0000

    Performance changes

--- Source/HelixToolkit.Wpf/Helpers/ContourHelper.cs
+++ Source/HelixToolkit.Wpf/Helpers/ContourHelper.cs
@@ -8,17 +8,21 @@ namespace HelixToolkit.Wpf
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Windows;
     using System.Windows.Media.Media3D;
 
     /// <summary>
-    /// Contour helper.
+    /// Provides functionality to calculate a contour slice through a 3 vertex facet.
     /// </summary>
     /// <remarks>
-    /// See http://paulbourke.net/papers/conrec/ for further information.
+    /// See <a href="http://paulbourke.net/papers/conrec/">CONREC</a> for further information.
     /// </remarks>
     internal class ContourHelper
     {
+        /// <summary>
+        /// Provides the indices for the various <see cref="ContourFacetResult"/> cases.
+        /// </summary>
         private static readonly IDictionary<ContourFacetResult, int[,]> ResultIndices
             = new Dictionary<ContourFacetResult, int[,]>
         {
@@ -31,27 +35,22 @@ namespace HelixToolkit.Wpf
         };
 
         /// <summary>
-        /// The original mesh.
-        /// </summary>
-        private readonly MeshGeometry3D originalMesh;
-
-        /// <summary>
-        /// The a.
+        /// The parameter 'a' of the plane equation.
         /// </summary>
         private readonly double a;
 
         /// <summary>
-        /// The b.
+        /// The parameter 'b' of the plane equation.
         /// </summary>
         private readonly double b;
 
         /// <summary>
-        /// The c.
+        /// The parameter 'c' of the plane equation.
         /// </summary>
         private readonly double c;
 
         /// <summary>
-        /// The d.
+        /// The parameter 'd' of the plane equation.
         /// </summary>
         private readonly double d;
 
@@ -80,7 +79,20 @@ namespace HelixToolkit.Wpf
         /// </summary>
         private int positionCount;
 
+        /// <summary>
+        /// Indicates whether the mesh uses texture coordinates.
+        /// </summary>
         private readonly bool hasTextureCoordinates;
+
+        /// <summary>
+        /// The original mesh positions.
+        /// </summary>
+        private readonly Point3D[] meshPositions;
+
+        /// <summary>
+        /// The original mesh texture coordinates.
+        /// </summary>
+        private readonly Point[] meshTextureCoordinates;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ContourHelper"/> class.
@@ -92,20 +104,26 @@ namespace HelixToolkit.Wpf
         /// The plane normal.
         /// </param>
         /// <param name="originalMesh">
-        /// The orginal mesh.
+        /// The original mesh.
+        /// </param>
+        /// <param name="hasTextureCoordinates">
+        /// Indicates whether texture coordinates need calculating.
         /// </param>
         public ContourHelper(Point3D planeOrigin, Vector3D planeNormal, MeshGeometry3D originalMesh, bool hasTextureCoordinates = false)
         {
-            this.originalMesh = originalMesh;
             this.hasTextureCoordinates = hasTextureCoordinates;
-            positionCount = originalMesh.Positions.Count;
+            this.positionCount = originalMesh.Positions.Count;
+
+            this.meshPositions = originalMesh.Positions.ToArray();
+            this.meshTextureCoordinates = originalMesh.TextureCoordinates.ToArray();
+
             // Determine the equation of the plane as
-            // Ax + By + Cz + D = 0
-            var l = Math.Sqrt(planeNormal.X * planeNormal.X + planeNormal.Y * planeNormal.Y + planeNormal.Z * planeNormal.Z);
-            a = planeNormal.X / l;
-            b = planeNormal.Y / l;
-            c = planeNormal.Z / l;
-            d = -(planeNormal.X * planeOrigin.X + planeNormal.Y * planeOrigin.Y + planeNormal.Z * planeOrigin.Z);
+            // ax + by + cz + d = 0
+            var l = Math.Sqrt((planeNormal.X * planeNormal.X) + (planeNormal.Y * planeNormal.Y) + (planeNormal.Z * planeNormal.Z));
+            this.a = planeNormal.X / l;
+            this.b = planeNormal.Y / l;
+            this.c = planeNormal.Z / l;
+            this.d = -((planeNormal.X * planeOrigin.X) + (planeNormal.Y * planeOrigin.Y) + (planeNormal.Z * planeOrigin.Z));
         }
 
         /// <summary>
@@ -176,8 +194,7 @@ namespace HelixToolkit.Wpf
         /// All triangle indices that are created, when 1 or more points fall below the contour plane.
         /// </param>
         public void ContourFacet(int index0, int index1, int index2,
-            out Point3D[] positions, out Point[] textureCoordinates,
-            out int[] triangleIndices)
+            out Point3D[] positions, out Point[] textureCoordinates, out int[] triangleIndices)
         {
             this.SetData(index0, index1, index2);
 
@@ -185,22 +202,22 @@ namespace HelixToolkit.Wpf
             switch (facetResult)
             {
                 case ContourFacetResult.ZeroOnly:
-                    triangleIndices = new [] { index0, positionCount++, positionCount++ };
+                    triangleIndices = new[] { index0, positionCount++, positionCount++ };
                     break;
                 case ContourFacetResult.OneAndTwo:
                     triangleIndices = new[] { index1, index2, positionCount, positionCount++, positionCount++, index1 };
                     break;
                 case ContourFacetResult.OneOnly:
-                    triangleIndices = new [] { index1, positionCount++, positionCount++ };
+                    triangleIndices = new[] { index1, positionCount++, positionCount++ };
                     break;
                 case ContourFacetResult.ZeroAndTwo:
                     triangleIndices = new[] { index2, index0, positionCount, positionCount++, positionCount++, index2 };
                     break;
                 case ContourFacetResult.TwoOnly:
-                    triangleIndices = new [] { index2, positionCount++, positionCount++ };
+                    triangleIndices = new[] { index2, positionCount++, positionCount++ };
                     break;
                 case ContourFacetResult.ZeroAndOne:
-                    triangleIndices = new [] { index0, index1, positionCount, positionCount++, positionCount++, index0 };
+                    triangleIndices = new[] { index0, index1, positionCount, positionCount++, positionCount++, index0 };
                     break;
                 case ContourFacetResult.All:
                     positions = new Point3D[0];
@@ -214,18 +231,19 @@ namespace HelixToolkit.Wpf
                     triangleIndices = new int[0];
                     return;
             }
+
             var facetIndices = ResultIndices[facetResult];
-            positions = new []
+            positions = new[]
             {
-                CreateNewPosition(facetIndices[0,0], facetIndices[0, 1]),
-                CreateNewPosition(facetIndices[1,0], facetIndices[1, 1])
+                this.CreateNewPosition(facetIndices[0, 0], facetIndices[0, 1]),
+                this.CreateNewPosition(facetIndices[1, 0], facetIndices[1, 1])
             };
-            if (hasTextureCoordinates)
+            if (this.hasTextureCoordinates)
             {
-                textureCoordinates = new []
+                textureCoordinates = new[]
                 {
-                    CreateNewTexture(facetIndices[0, 0], facetIndices[0, 1]),
-                    CreateNewTexture(facetIndices[1, 0], facetIndices[1, 1])
+                    this.CreateNewTexture(facetIndices[0, 0], facetIndices[0, 1]),
+                    this.CreateNewTexture(facetIndices[1, 0], facetIndices[1, 1])
                 };
             }
             else
@@ -234,92 +252,144 @@ namespace HelixToolkit.Wpf
             }
         }
 
+        /// <summary>
+        /// Gets the <see cref="ContourFacetResult"/> for the current facet.
+        /// </summary>
+        /// <returns>a facet result.</returns>
         private ContourFacetResult GetContourFacet()
         {
-            if (IsSideAlone(0))
+            if (this.IsSideAlone(0))
             {
-                return sides[0] > 0 ? ContourFacetResult.ZeroOnly : ContourFacetResult.OneAndTwo;
+                return this.sides[0] > 0 ? ContourFacetResult.ZeroOnly : ContourFacetResult.OneAndTwo;
             }
-            if (IsSideAlone(1))
+
+            if (this.IsSideAlone(1))
             {
-                return sides[1] > 0 ? ContourFacetResult.OneOnly : ContourFacetResult.ZeroAndTwo;
+                return this.sides[1] > 0 ? ContourFacetResult.OneOnly : ContourFacetResult.ZeroAndTwo;
             }
-            if (IsSideAlone(2))
+
+            if (this.IsSideAlone(2))
             {
-                return sides[2] > 0 ? ContourFacetResult.TwoOnly : ContourFacetResult.ZeroAndOne;
+                return this.sides[2] > 0 ? ContourFacetResult.TwoOnly : ContourFacetResult.ZeroAndOne;
             }
-            if (AllSidesBelowContour())
+
+            if (this.AllSidesBelowContour())
             {
                 return ContourFacetResult.All;
             }
+
             return ContourFacetResult.None;
         }
 
+        /// <summary>
+        /// Initializes the facet data and calculates the <see cref="sides"/> values from the specified triangle indices. 
+        /// </summary>
+        /// <param name="index0">The first triangle index of the facet.</param>
+        /// <param name="index1">The second triangle index of the facet.</param>
+        /// <param name="index2">The third triangle index of the facet.</param>
         private void SetData(int index0, int index1, int index2)
         {
-            indices[0] = index0;
-            indices[1] = index1;
-            indices[2] = index2;
+            this.indices[0] = index0;
+            this.indices[1] = index1;
+            this.indices[2] = index2;
 
-            points[0] = originalMesh.Positions[index0];
-            points[1] = originalMesh.Positions[index1];
-            points[2] = originalMesh.Positions[index2];
+            this.points[0] = this.meshPositions[index0];
+            this.points[1] = this.meshPositions[index1];
+            this.points[2] = this.meshPositions[index2];
 
-            if (hasTextureCoordinates)
+            if (this.hasTextureCoordinates)
             {
-                textures[0] = originalMesh.TextureCoordinates[index0];
-                textures[1] = originalMesh.TextureCoordinates[index1];
-                textures[2] = originalMesh.TextureCoordinates[index2];
+                this.textures[0] = this.meshTextureCoordinates[index0];
+                this.textures[1] = this.meshTextureCoordinates[index1];
+                this.textures[2] = this.meshTextureCoordinates[index2];
             }
 
-            sides[0] = a * points[0].X + b * points[0].Y + c * points[0].Z + d;
-            sides[1] = a * points[1].X + b * points[1].Y + c * points[1].Z + d;
-            sides[2] = a * points[2].X + b * points[2].Y + c * points[2].Z + d;
+            this.sides[0] = (this.a * this.points[0].X) + (this.b * this.points[0].Y) + (this.c * this.points[0].Z) + this.d;
+            this.sides[1] = (this.a * this.points[1].X) + (this.b * this.points[1].Y) + (this.c * this.points[1].Z) + this.d;
+            this.sides[2] = (this.a * this.points[2].X) + (this.b * this.points[2].Y) + (this.c * this.points[2].Z) + this.d;
         }
 
+        /// <summary>
+        /// Calculates the position at the plane intersection for the side specified by two triangle indices.
+        /// </summary>
+        /// <param name="index0">The first index.</param>
+        /// <param name="index1">The second index.</param>
+        /// <returns>The interpolated position.</returns>
         private Point3D CreateNewPosition(int index0, int index1)
         {
-            var firstPoint = points[index0];
-            var secondPoint = points[index1];
-            var firstSide = sides[index0];
-            var secondSide = sides[index1];
+            var firstPoint = this.points[index0];
+            var secondPoint = this.points[index1];
+            var firstSide = this.sides[index0];
+            var secondSide = this.sides[index1];
             return new Point3D(
                 CalculatePoint(firstPoint.X, secondPoint.X, firstSide, secondSide),
                 CalculatePoint(firstPoint.Y, secondPoint.Y, firstSide, secondSide),
                 CalculatePoint(firstPoint.Z, secondPoint.Z, firstSide, secondSide));
         }
 
+        /// <summary>
+        /// Calculates the texture coordinate at the plane intersection for the side specified by two triangle indices.
+        /// </summary>
+        /// <param name="index0">The first index.</param>
+        /// <param name="index1">The second index.</param>
+        /// <returns>The interpolated texture coordinate.</returns>
         private Point CreateNewTexture(int index0, int index1)
         {
-            var firstTexture = textures[index0];
-            var secondTexture = textures[index1];
-            var firstSide = sides[index0];
-            var secondSide = sides[index1];
+            var firstTexture = this.textures[index0];
+            var secondTexture = this.textures[index1];
+            var firstSide = this.sides[index0];
+            var secondSide = this.sides[index1];
+
             return new Point(
                 CalculatePoint(firstTexture.X, secondTexture.X, firstSide, secondSide),
                 CalculatePoint(firstTexture.Y, secondTexture.Y, firstSide, secondSide));
         }
 
+        /// <summary>
+        /// Calculates a new point coordinate.
+        /// </summary>
+        /// <param name="firstPoint">
+        /// The first point coordinate.
+        /// </param>
+        /// <param name="secondPoint">
+        /// The second point coordinate.
+        /// </param>
+        /// <param name="firstSide">
+        /// The first side.
+        /// </param>
+        /// <param name="secondSide">
+        /// The second side.
+        /// </param>
+        /// <returns></returns>
         private static double CalculatePoint(double firstPoint, double secondPoint, double firstSide, double secondSide)
         {
             return firstPoint - (firstSide * (secondPoint - firstPoint) / (secondSide - firstSide));
         }
 
+        /// <summary>
+        /// Determines whether the vertex at the specified index is at the opposite side of the other two vertices.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <returns><c>true</c> if the vertex is on its own side.</returns>
         private bool IsSideAlone(int index)
         {
             Func<int, int> getNext = i => i + 1 > 2 ? 0 : i + 1;
 
             var firstSideIndex = getNext(index);
             var secondSideIndex = getNext(firstSideIndex);
-            return sides[index] * sides[firstSideIndex] < 0
-                && sides[index] * sides[secondSideIndex] < 0;
+            return this.sides[index] * this.sides[firstSideIndex] < 0
+                && this.sides[index] * this.sides[secondSideIndex] < 0;
         }
 
+        /// <summary>
+        /// Determines whether all sides of the facet are below the contour.
+        /// </summary>
+        /// <returns><c>true</c> if all sides are below the contour.</returns>
         private bool AllSidesBelowContour()
         {
-            return sides[0] >= 0
-                && sides[1] >= 0
-                && sides[2] >= 0;
+            return this.sides[0] >= 0
+                && this.sides[1] >= 0
+                && this.sides[2] >= 0;
         }
     }
 }
