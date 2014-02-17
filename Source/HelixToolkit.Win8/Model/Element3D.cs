9d666d77e10404e23c08c5a32c8e44fe8716cb07
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-02-17 22:42:06 +0100

    Merge HelixToolkit.Win8 from sharpdx fork

---
+++ Source/HelixToolkit.Win8/Model/Element3D.cs
@@ -0,0 +1,28 @@
+﻿namespace HelixToolkit.Win8
+{
+    using HelixToolkit.Win8.CommonDX;
+
+    using Windows.UI.Xaml;
+
+    /// <summary>
+    /// Base class for 3D elements.
+    /// </summary>
+    public abstract class Element3D : FrameworkElement
+    {
+        /// <summary>
+        /// Initializes the element.
+        /// </summary>
+        /// <param name="deviceManager">The device manager.</param>
+        public virtual void Initialize(DeviceManager deviceManager)
+        {
+        }
+
+        /// <summary>
+        /// Renders the element.
+        /// </summary>
+        /// <param name="render">The render.</param>
+        public virtual void Render(TargetBase render)
+        {
+        }
+    }
+}
