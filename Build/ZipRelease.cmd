48aef0b6b92240d5ae6eae477ca11d192a202e2b
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-04-29 11:17:27 +0200

    Remove examples from Codeplex Release package.

--- Build/ZipRelease.cmd
+++ Build/ZipRelease.cmd
@@ -1,6 +1,13 @@
-call DeletePdbFiles.cmd ..\Output\NET45\Examples
-call DeletePdbFiles.cmd ..\Output\NET40\Examples
+mkdir ..\Output\Release
+mkdir ..\Output\Release\NET40
+mkdir ..\Output\Release\NET45
 
-del /S /Q ..\Output\NET40\Examples
+copy ..\Output\NET45\HelixToolkit.??? ..\Output\Release
+copy ..\Output\NET45\HelixToolkit.Wpf.??? ..\Output\Release\NET45
+copy ..\Output\NET40\HelixToolkit.Wpf.??? ..\Output\Release\NET40
+copy ..\Output\NET45\HelixToolkit.Wpf.Input.??? ..\Output\Release\NET45
+copy ..\Output\NET40\HelixToolkit.Wpf.Input.??? ..\Output\Release\NET40
+copy ..\Output\NET45\HelixToolkit.Wpf.SharpDX.??? ..\Output\Release\NET45
+copy ..\Output\NET40\HelixToolkit.Wpf.SharpDX.??? ..\Output\Release\NET40
 
-"C:\Program Files\7-Zip\7z.exe" a -r ..\Output\HelixToolkit-%1.zip ..\Output\*.* > ZipRelease.log
+"C:\Program Files\7-Zip\7z.exe" a -r ..\Output\HelixToolkit-%1.zip ..\Output\Release\*.* > ZipRelease.log
