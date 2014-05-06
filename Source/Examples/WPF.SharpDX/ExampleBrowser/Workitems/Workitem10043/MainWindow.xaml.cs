2699ca5506f310a0a4bea6270916ca0868fd345d
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-05-06 22:12:01 +0200

    SharpDX: Add ExampleBrowser Move Workitem10043 project into ExampleBrowser Remove Workitem10043 sln

--- Source/Examples/WPF.SharpDX/ExampleBrowser/Workitems/Workitem10043/MainWindow.xaml.cs
+++ Source/Examples/WPF.SharpDX/ExampleBrowser/Workitems/Workitem10043/MainWindow.xaml.cs
@@ -1,16 +1,18 @@
-﻿namespace SimpleDemo
+﻿namespace Workitem10043
 {
     using System.Windows;
+
+    using ExampleBrowser;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    [Example("Issue 10043", "Viewport3DX inside a TabControl.")]
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            this.InitializeComponent();                                  
+            this.InitializeComponent();
         }
     }
-
 }
