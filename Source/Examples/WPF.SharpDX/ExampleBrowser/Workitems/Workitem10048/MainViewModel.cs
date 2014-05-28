660060711e6a50a1c3768ac1ec3c45731fa287f1
smischke <smischke@FCS-NB513.eckelmann.group>
2014-05-28 16:58:11 +0200

    SharpDX: HitTesting for LineGeometryModel3D (workitem 10048 and 10052)

---
+++ Source/Examples/WPF.SharpDX/ExampleBrowser/Workitems/Workitem10048/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿ namespace Workitem10048
+{
+    using DemoCore;
+
+    using HelixToolkit.Wpf.SharpDX;
+
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            // titles
+            this.Title = "Simple Demo (Workitem 10048 and 10052)";
+            this.SubTitle = "Select lines with left mouse button.\nRotate or zoom around a point on a line if the cursor is above one.";
+
+            // default render technique
+            this.RenderTechnique = Techniques.RenderBlinn;
+        }
+    }
+}
---
+++ Source/Examples/WPF.SharpDX/ExampleBrowser/Workitems/Workitem10048/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Workitem10048
+{
+    using System.Windows;
+
+    using ExampleBrowser;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    [Example("Issue 10048 and 10052", "SharpDX: Implement hit testing for lines.")]
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();                                  
+        }
+    }
+}
