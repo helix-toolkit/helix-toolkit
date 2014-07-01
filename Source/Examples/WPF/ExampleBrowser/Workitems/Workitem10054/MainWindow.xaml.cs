2bacd2605a2c18926e56975b343eccd3987b1e4b
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-07-01 16:36:30 +0200

    #10054: add demo

---
+++ Source/Examples/WPF/ExampleBrowser/Workitems/Workitem10054/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Workitem10054
+{
+    /// <summary>
+    /// Interaction logic for the main window.
+    /// </summary>
+    [ExampleBrowser.Example("Work item 10054", "TubeVisual3D")]
+    public partial class MainWindow
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
