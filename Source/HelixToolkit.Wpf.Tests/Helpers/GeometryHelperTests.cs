26cae5f7b8f4cf71b4e10d633b2efc822bebb8e6
Edward Zhu <edward.zr.zhu@gmail.com>
2014-08-22 17:21:20 +0800

    Add the Unit test for intersection check and point location check.

---
+++ Source/HelixToolkit.Wpf.Tests/Helpers/GeometryHelperTests.cs
@@ -0,0 +1,74 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GeometryHelperTests.cs" company="Helix 3D Toolkit">
+//   http://helixtoolkit.codeplex.com, license: MIT
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HelixToolkit.Wpf.Tests
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+
+    using NUnit.Framework;
+
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
+    [TestFixture]
+    public class GeometryHelperTests
+    {
+        [Test]
+        public void TriangleInsideRectTest()
+        {
+            var point1 = new Point(0, 0);
+            var point2 = new Point(2, 0);
+            var point3 = new Point(0, 3);
+            var rect = new Rect(new Point(0, 0), new Size(2, 2));
+
+            Assert.IsFalse(GeometryHelper.TriangleInsideRect(point1, point2, point3, rect));
+            rect.Size = new Size(2, 4);
+            Assert.IsTrue(GeometryHelper.TriangleInsideRect(point1, point2, point3, rect));
+            rect.Size = new Size(2, 3);
+            Assert.IsTrue(GeometryHelper.TriangleInsideRect(point1, point2, point3, rect));
+            rect.Location = new Point(0.5, 1);
+            rect.Size = new Size(0.25, 0.25);
+            Assert.IsFalse(GeometryHelper.TriangleInsideRect(point1, point2, point3, rect));
+            rect.Location = new Point(-1, -1);
+            rect.Size = new Size(5, 5);
+            Assert.IsTrue(GeometryHelper.TriangleInsideRect(point1, point2, point3, rect));
+        }
+
+        [Test]
+        public void PointInTriangleTest()
+        {
+            var point1 = new Point(0, 0);
+            var point2 = new Point(2, 0);
+            var point3 = new Point(0, 3);
+            var point4 = new Point(3, 0);
+
+            var p = new Point(1, 1);
+
+            Assert.IsTrue(GeometryHelper.PointInTriangle(p, point1, point2, point3));
+            p = new Point(2, 3);
+            Assert.IsFalse(GeometryHelper.PointInTriangle(p, point1, point2, point3));
+            p = new Point(1, 0);
+            Assert.IsFalse(GeometryHelper.PointInTriangle(p, point1, point2, point3));
+            Assert.IsFalse(GeometryHelper.PointInTriangle(point1, point1, point2, point3));
+            Assert.IsFalse(GeometryHelper.PointInTriangle(p, point1, point2, point4));
+            Assert.IsFalse(GeometryHelper.PointInTriangle(point3, point1, point2, point4));
+        }
+
+        [Test]
+        public void IntersectTest()
+        {
+            var a1 = new Point(0, 0);
+            var a2 = new Point(1, 0);
+            var b1 = new Point(1, 1);
+            var b2 = new Point(2, 1);
+
+            Assert.IsFalse(GeometryHelper.Intersect(a1, a2, b1, b2));
+            b2 = new Point(0, 0);
+            Assert.IsTrue(GeometryHelper.Intersect(a1, a2, b1, b2));
+            b2 = new Point(-1, 0);
+            Assert.IsFalse(GeometryHelper.Intersect(a1, a2, b1, b2));
+        }
+    }
+}
--- Source/HelixToolkit.Wpf/Helpers/Viewport3DHelper.cs
+++ Source/HelixToolkit.Wpf/Helpers/Viewport3DHelper.cs
@@ -234,12 +234,12 @@ namespace HelixToolkit.Wpf
                                 switch (mode)
                                 {
                                     case SelectionHitMode.Inside:
-                                        status = status && InsideRect(a, b, c, rectangle);
+                                        status = status && GeometryHelper.TriangleInsideRect(a, b, c, rectangle);
                                         break;
                                     case SelectionHitMode.Touch:
                                         status = status
-                                                 || InsideRect(a, b, c, rectangle) || Intersect(a, b, c, rectangle)
-                                                 || InsideTriangle(a, b, c, rectangle);
+                                                 || GeometryHelper.TriangleInsideRect(a, b, c, rectangle) || GeometryHelper.Intersect(a, b, c, rectangle)
+                                                 || GeometryHelper.RectInsideTriangle(a, b, c, rectangle);
                                         break;
                                 }
 
@@ -942,120 +942,6 @@ namespace HelixToolkit.Wpf
                     }
                 });
             return count;
-        }
-
-        /// <summary>
-        /// Check whether the triangle is in the rectangle.
-        /// </summary>
-        /// <param name="a">The vertex a of triangle</param>
-        /// <param name="b">The vertex b of triangle</param>
-        /// <param name="c">The vertex c of triangle</param>
-        /// <param name="rect">The rectangle</param>
-        /// <returns>
-        /// True if the triangle is inside the rectangle.
-        /// </returns>
-        private static bool InsideRect(Point a, Point b, Point c, Rect rect)
-        {
-            return rect.Contains(a) && rect.Contains(b) && rect.Contains(c);
-        }
-
-        /// <summary>
-        /// Check whether the rectangle is inside the triangle.
-        /// </summary>
-        /// <param name="a">The vertex a of triangle</param>
-        /// <param name="b">The vertex b of triangle</param>
-        /// <param name="c">The vertex c of triangle</param>
-        /// <param name="rect">The rectangle.</param>
-        /// <returns>
-        /// True if the rectangle is inside triangle. Otherwise, it returns false.
-        /// </returns>
-        private static bool InsideTriangle(Point a, Point b, Point c, Rect rect)
-        {
-            return PointInTriangle(rect.TopLeft, a, b, c) && PointInTriangle(rect.TopRight, a, b, c)
-                   && PointInTriangle(rect.BottomLeft, a, b, c) && PointInTriangle(rect.BottomRight, a, b, c);
-        }
-
-        /// <summary>
-        /// Check whether the point is in the triangle. See: http://stackoverflow.com/questions/2049582/how-to-determine-a-point-in-a-triangle
-        /// </summary>
-        /// <param name="p">The point to be checked.</param>
-        /// <param name="p0">The vertex p0 of the triangle.</param>
-        /// <param name="p1">The vertex p1 of the triangle.</param>
-        /// <param name="p2">The vertex p2 of the triangle.</param>
-        /// <returns>
-        /// True if the piont is in the triangle. Otherwise, it returns false.
-        /// </returns>
-        public static bool PointInTriangle(Point p, Point p0, Point p1, Point p2)
-        {
-            var s = p0.Y * p2.X - p0.X * p2.Y + (p2.Y - p0.Y) * p.X + (p0.X - p2.X) * p.Y;
-            var t = p0.X * p1.Y - p0.Y * p1.X + (p0.Y - p1.Y) * p.X + (p1.X - p0.X) * p.Y;
-
-            if ((s < 0) != (t < 0))
-                return false;
-
-            var a = -p1.Y * p2.X + p0.Y * (p2.X - p1.X) + p0.X * (p1.Y - p2.Y) + p1.X * p2.Y;
-            if (a < 0.0)
-            {
-                s = -s;
-                t = -t;
-                a = -a;
-            }
-
-            return s > 0 && t > 0 && (s + t) < a;
-        }
-
-        /// <summary>
-        /// Intersect of two lines.
-        /// </summary>
-        /// <param name="a1">One vertex of line a.</param>
-        /// <param name="a2">The other vertex of the line a.</param>
-        /// <param name="b1">One vertex of line b.</param>
-        /// <param name="b2">The other vertex of the line b.</param>
-        /// <returns>
-        /// True, if the two lines are crossed. Otherwise, it returns false.
-        /// </returns>
-        private static bool Intersect(Point a1, Point a2, Point b1, Point b2)
-        {
-            if (b1 == b2 || a1 == a2)
-            {
-                return false;
-            }
-
-            if (((a2.X - a1.X) * (b1.Y - a1.Y) - (b1.X - a1.X) * (a2.Y - a1.Y))
-                * ((a2.X - a1.X) * (b2.Y - a1.Y) - (b2.X - a1.X) * (a2.Y - a1.Y)) > 0)
-            {
-                return false;
-            }
-
-            if (((b2.X - b1.X) * (a1.Y - b1.Y) - (a1.X - b1.X) * (b2.Y - b1.Y))
-                * ((b2.X - b1.X) * (a2.Y - b1.Y) - (a2.X - b1.X) * (b2.Y - b1.Y)) > 0)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        /// Intersect of the point with the rectangle.
-        /// </summary>
-        /// <param name="a">The vertex a of the triangle.</param>
-        /// <param name="b">The vertex b of the triangle.</param>
-        /// <param name="c">The vertex c of the triangle.</param>
-        /// <param name="rect">The rectangle to be checked.</param>
-        /// <returns>
-        /// True, if the triangle intersect with the rectangle. Otherwise, it returns false.
-        /// </returns>
-        private static bool Intersect(Point a, Point b, Point c, Rect rect)
-        {
-            return Intersect(a, b, rect.BottomLeft, rect.BottomRight) || Intersect(a, b, rect.BottomLeft, rect.TopLeft)
-                   || Intersect(a, b, rect.TopLeft, rect.TopRight) || Intersect(a, b, rect.TopRight, rect.BottomRight)
-                   || Intersect(b, c, rect.BottomLeft, rect.BottomRight)
-                   || Intersect(b, c, rect.BottomLeft, rect.TopLeft) || Intersect(b, c, rect.TopLeft, rect.TopRight)
-                   || Intersect(b, c, rect.TopRight, rect.BottomRight)
-                   || Intersect(c, a, rect.BottomLeft, rect.BottomRight)
-                   || Intersect(c, a, rect.BottomLeft, rect.TopLeft) || Intersect(c, a, rect.TopLeft, rect.TopRight)
-                   || Intersect(c, a, rect.TopRight, rect.BottomRight);
         }
 
         /// <summary>
