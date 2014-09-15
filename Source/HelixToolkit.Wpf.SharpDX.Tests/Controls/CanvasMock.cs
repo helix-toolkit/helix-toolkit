82f19ee350eab6b10745d5d7e38938817db57258
Der_Meister <der@heavysoft.ru>
2014-09-15 21:34:06 +0800

    Added test.

---
+++ Source/HelixToolkit.Wpf.SharpDX.Tests/Controls/CanvasMock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SharpDX;
+using SharpDX.Direct3D11;
+
+namespace HelixToolkit.Wpf.SharpDX.Tests.Controls
+{
+    class CanvasMock : IRenderHost
+    {
+        public CanvasMock()
+        {
+            Device = EffectsManager.Device;
+            RenderTechnique = Techniques.RenderPhong;
+        }
+
+        public Device Device { get; private set; }
+        public Color4 ClearColor { get; private set; }
+        public bool IsShadowMapEnabled { get; private set; }
+        public bool IsMSAAEnabled { get; private set; }
+        public IRenderer Renderable { get; private set; }
+        public void SetDefaultRenderTargets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public RenderTechnique RenderTechnique { get; private set; }
+        public double ActualHeight { get; private set; }
+        public double ActualWidth { get; private set; }
+
+        public void SetDefaultColorTargets(DepthStencilView dsv)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/HelixToolkit.Wpf.SharpDX.Tests/Elements3D/MeshGeometryModel3DTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HelixToolkit.Wpf.SharpDX.Tests.Controls;
+using NUnit.Framework;
+
+namespace HelixToolkit.Wpf.SharpDX.Tests.Elements3D
+{
+    [TestFixture]
+    class MeshGeometryModel3DTests
+    {
+        [Test, STAThread]
+        public void CreateDefaultVertexArrayForTriangle()
+        {
+            var reader = new ObjReader();
+            var objects = reader.Read(@"Models\obj\Triangle.obj");
+
+            Assert.AreEqual(1, objects.Count);
+
+            var geometry = objects[0].Geometry;
+            var model = new MeshGeometryModel3D { Geometry = geometry };
+
+            var canvas = new CanvasMock();
+            model.Attach(canvas);
+
+            Assert.AreEqual(true, model.IsAttached);
+        }
+    }
+}
