c059472d284480c7e2795b982f4d7ed2330dd503
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-10-06 21:31:00 +0200

    Add more options to headlight demo

--- Source/Examples/WPF/ExampleBrowser/Examples/Lights/MainWindow.xaml.cs
+++ Source/Examples/WPF/ExampleBrowser/Examples/Lights/MainWindow.xaml.cs
@@ -9,7 +9,12 @@
 
 namespace LightsDemo
 {
+    using System.Windows.Media;
+    using System.Windows.Media.Media3D;
+
     using ExampleBrowser;
+
+    using HelixToolkit.Wpf;
 
     using PropertyTools;
 
@@ -36,6 +41,10 @@ namespace LightsDemo
             private double ambient;
             private double azimuth = 30;
             private double altitude = 60;
+            private double specularBrightness = 0.7;
+            private double specularPower = 30;
+            private double headlightLateralPosition = 30;
+            private double headlightVerticalPosition = 10;
 
             public double Ambient
             {
@@ -73,6 +82,61 @@ namespace LightsDemo
                 set { this.SetValue(ref this.enableHeadlight, value, () => this.EnableHeadlight); }
             }
 
+            public double SpecularBrightness
+            {
+                get { return this.specularBrightness; }
+                set { this.SetValue(ref this.specularBrightness, value, () => this.SpecularBrightness); RaisePropertyChanged(() => this.GreenMaterial); RaisePropertyChanged(() => this.BlueMaterial); }
+            }
+
+            public double SpecularPower
+            {
+                get { return this.specularPower; }
+                set { this.SetValue(ref this.specularPower, value, () => this.SpecularPower); RaisePropertyChanged(() => this.GreenMaterial); RaisePropertyChanged(() => this.BlueMaterial); }
+            }
+
+            public double HeadlightLateralPosition
+            {
+                get { return this.headlightLateralPosition; }
+                set { this.SetValue(ref this.headlightLateralPosition, value, () => this.HeadlightLateralPosition); RaisePropertyChanged(() => this.Headlight1); RaisePropertyChanged(() => this.Headlight2); }
+            }
+
+            public double HeadlightVerticalPosition
+            {
+                get { return this.headlightVerticalPosition; }
+                set { this.SetValue(ref this.headlightVerticalPosition, value, () => this.HeadlightVerticalPosition); RaisePropertyChanged(() => this.Headlight1); RaisePropertyChanged(() => this.Headlight2); }
+            }
+
+            public Material GreenMaterial
+            {
+                get
+                {
+                    return MaterialHelper.CreateMaterial(
+                        Brushes.Green,
+                        this.specularBrightness,
+                        this.specularPower);
+                }
+            }
+
+            public Material BlueMaterial
+            {
+                get
+                {
+                    return MaterialHelper.CreateMaterial(
+                        Brushes.DodgerBlue,
+                        this.specularBrightness,
+                        this.specularPower);
+                }
+            }
+
+            public Point3D Headlight1
+            {
+                get { return new Point3D(this.HeadlightLateralPosition, 0, this.HeadlightVerticalPosition); }
+            }
+
+            public Point3D Headlight2
+            {
+                get { return new Point3D(-this.HeadlightLateralPosition, 0, this.HeadlightVerticalPosition); }
+            }
         }
     }
 }
