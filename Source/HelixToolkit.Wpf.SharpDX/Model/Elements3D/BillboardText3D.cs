3954a389a15711c2ee180ce76c82f87f1ea45679
Benglin (Ben) Goh <benglin@outlook.com>
2014-11-17 15:15:32 -0800

    Introduced skeleton classes: BillboardText3D, BillboardTextModel3D

---
+++ Source/HelixToolkit.Wpf.SharpDX/Model/Elements3D/BillboardText3D.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelixToolkit.Wpf.SharpDX.Model.Elements3D
+{
+    class BillboardText3D : MeshGeometry3D
+    {
+    }
+}
---
+++ Source/HelixToolkit.Wpf.SharpDX/Model/Elements3D/BillboardTextModel3D.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelixToolkit.Wpf.SharpDX.Model.Elements3D
+{
+    class BillboardTextModel3D : MeshGeometryModel3D
+    {
+    }
+}
