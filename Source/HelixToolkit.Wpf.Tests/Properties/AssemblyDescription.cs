32a4879d50f6cbe687ceaff71716764e9cb492ee
Oystein Bjorke <oystein.bjorke@gmail.com>
2016-09-08 00:43:18 +0200

    Rename GlobalAssemblyInfo

---
+++ Source/HelixToolkit.Wpf.Tests/Properties/AssemblyDescription.cs
@@ -0,0 +1,10 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyDescription.cs" company="Helix Toolkit">
+//   Copyright (c) 2014 Helix Toolkit contributors
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("Helix Toolkit WPF Unit Tests")]
+[assembly: AssemblyDescription("Unit tests for HelixToolkit.Wpf")]
---
+++ Source/HelixToolkit/Properties/AssemblyDescription.cs
@@ -0,0 +1,10 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyDescription.cs" company="Helix Toolkit">
+//   Copyright (c) 2014 Helix Toolkit contributors
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("Helix 3D Toolkit core library")]
+[assembly: AssemblyDescription("Core library containing classes shared by different platforms.")]
