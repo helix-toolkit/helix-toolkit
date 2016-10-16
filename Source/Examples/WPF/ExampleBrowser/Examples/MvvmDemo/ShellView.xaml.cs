62dd8db9247053e659edb33b7a59dac5118b9b86
Oystein Bjorke <oystein.bjorke@gmail.com>
2016-10-16 22:23:16 +0200

    Move MvvmDemo to ExampleBrowser

---
+++ Source/Examples/WPF/ExampleBrowser/Examples/MvvmDemo/ShellView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MvvmDemo
+{
+    using ExampleBrowser;
+
+    [Example(null, "Add and remove with MVVM.")]
+    public partial class ShellView
+    {
+        public ShellView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
