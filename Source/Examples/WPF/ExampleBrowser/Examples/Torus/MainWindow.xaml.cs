b14bf155a1287ba36ff9f8169472d875a0244ff9
Franz Spitaler <spitefr@gmail.com>
2016-10-26 06:48:37 +0200

    Added a TorusVisual3D and an Example Project to demonstrate it's use (issue #180, #74) (#318)
    A TorusVisual3D was added. It uses the enhanced MeshBuilder to generate
    the Geometry for a Torus. The Torus handles special cases like
    self-intersection and restricts the values to a valid range.
    A simple demo was also added to the example browser to demonstrate the
    usage.

---
+++ Source/Examples/WPF/ExampleBrowser/Examples/Torus/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Helix Toolkit">
+//   Copyright (c) 2016 Helix Toolkit contributors
+// </copyright>
+// <summary>
+//   Interaction logic for MainWindow.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TorusDemo
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Media.Media3D;
+
+    using ExampleBrowser;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    [Example(null, "Shows three torus objects using the TorusVisual3D.")]
+    public partial class MainWindow : Window
+    {
+        public Transform3D Transform1 = new TranslateTransform3D(new Vector3D(-4, 0, 0));
+        public Transform3D Transform3 = new TranslateTransform3D(new Vector3D(4, 0, 0));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            torus1.Transform = Transform1;
+            torus3.Transform = Transform3;
+            viewPort.Camera.Position = new Point3D(0, 0, 10);
+        }
+    }
+}
