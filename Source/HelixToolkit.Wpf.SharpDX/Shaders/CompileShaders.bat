e5b387e25c4486c12c390e7ec4272698947bee0e
Lunci <lunci.app@gmail.com>
2016-12-20 15:07:24 -0800

    Update _deferred.bfx to latest build. Include deferred.fx build in build script.

--- Source/HelixToolkit.Wpf.SharpDX/Shaders/CompileShaders.bat
+++ Source/HelixToolkit.Wpf.SharpDX/Shaders/CompileShaders.bat
@@ -4,5 +4,6 @@ pushd %~dp0
 rem fxc /Od /Zi /T fx_5_0 /I ./../ /Fo ..\Resources\_default.bfx Default.fx
 rem fxc /T fx_5_0 /I ./../ /Fo ..\Resources\_default.bfx Default.fx
 fxc /T fx_5_0 /I ./../ /Fo ..\Resources\_default.bfx Tessellation.fx
+fxc /D SSAO /D DEFERRED_MSAA /T fx_5_0 /I ./../ /Fo ..\Resources\_deferred.bfx Deferred.fx
 popd
 pause
