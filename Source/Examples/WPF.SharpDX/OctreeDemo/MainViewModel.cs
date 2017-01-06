524902e441f4a5c753f00f022ed72b9b15a1d992
holance <holance@gmail.com>
2017-01-06 01:34:37 -0800

    Added octree tester/demo

---
+++ Source/Examples/WPF.SharpDX/OctreeDemo/MainViewModel.cs
@@ -0,0 +1,246 @@
+﻿using DemoCore;
+using HelixToolkit.SharpDX.Shared.Utilities;
+using HelixToolkit.Wpf.SharpDX;
+using HelixToolkit.Wpf.SharpDX.Core;
+using SharpDX;
+using SharpDX.Direct3D11;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using Media3D = System.Windows.Media.Media3D;
+
+namespace OctreeDemo
+{    
+    public class MainViewModel : BaseViewModel
+    {
+        private Vector3 light1Direction = new Vector3();
+        public Vector3 Light1Direction
+        {
+            set
+            {
+                if (light1Direction != value)
+                {
+                    light1Direction = value;
+                    OnPropertyChanged();
+                }
+            }
+            get
+            {
+                return light1Direction;
+            }
+        }
+        private FillMode fillMode = FillMode.Solid;
+        public FillMode FillMode
+        {
+            set
+            {
+                fillMode = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return fillMode;
+            }
+        }
+
+        private bool showWireframe = false;
+        public bool ShowWireframe
+        {
+            set
+            {
+                showWireframe = value;
+                OnPropertyChanged();
+                if (showWireframe)
+                {
+                    FillMode = FillMode.Wireframe;
+                }
+                else
+                {
+                    FillMode = FillMode.Solid;
+                }
+            }
+            get
+            {
+                return showWireframe;
+            }
+        }
+        public Color4 Light1Color { get; set; }
+        public PhongMaterial ModelMaterial { get; set; }
+
+        public PhongMaterial InnerModelMaterial { get; set; }
+
+        //public PhongMaterial OtherMaterial { set; get; }
+        public MeshGeometry3D Model { get; private set; }
+        public MeshGeometry3D InnerModel { get; private set; }
+
+        //public MeshGeometry3D Other { get; private set; }
+        public Color4 AmbientLightColor { get; set; }
+
+        public Color LineColor { set; get; }
+
+        public LineGeometry3D OctreeModel { set; get; }
+
+        private Media3D.Vector3D camLookDir = new Media3D.Vector3D(-10, -10, -10);
+        public Media3D.Vector3D CamLookDir
+        {
+            set
+            {
+                if (camLookDir != value)
+                {
+                    camLookDir = value;
+                    OnPropertyChanged();
+                }
+            }
+            get
+            {
+                return camLookDir;
+            }
+        }
+
+        private Vector3Collection initialPosition;
+        private IntCollection initialIndicies;
+        private Random rnd = new Random();
+        private bool isRemoving = true;
+        private int removedIndex = 0;
+        public MainViewModel()
+        {            // titles
+            this.Title = "DynamicTexture Demo";
+            this.SubTitle = "WPF & SharpDX";
+            RenderTechniquesManager = new DefaultRenderTechniquesManager();
+            RenderTechnique = RenderTechniquesManager.RenderTechniques[DefaultRenderTechniqueNames.Blinn];
+            EffectsManager = new DefaultEffectsManager(RenderTechniquesManager);
+            this.Camera = new HelixToolkit.Wpf.SharpDX.PerspectiveCamera { Position = new Media3D.Point3D(10, 10, 10),
+                LookDirection = new Media3D.Vector3D(-10, -10, -10), UpDirection = new Media3D.Vector3D(0, 1, 0) };
+            this.Light1Color = (Color4)Color.White;
+            this.Light1Direction = new Vector3(-10, -10, -10);
+            this.AmbientLightColor = new Color4(0.2f, 0.2f, 0.2f, 1.0f);
+            SetupCameraBindings(this.Camera);
+
+            var b2 = new MeshBuilder(true, true, true);
+            b2.AddSphere(new Vector3(0f, 0f, 0f), 4, 64, 64);
+            this.Model = b2.ToMeshGeometry3D();
+            this.InnerModel = new MeshGeometry3D()
+            {
+                Indices = Model.Indices, Positions = Model.Positions,
+                Normals = new Vector3Collection(Model.Normals.Select(x => { return x * -1; })),
+                TextureCoordinates = Model.TextureCoordinates,
+                Tangents = Model.Tangents,
+                BiTangents = Model.BiTangents
+            };
+            var uri = new System.Uri(@"test.png", System.UriKind.RelativeOrAbsolute);
+
+            this.ModelMaterial = new PhongMaterial
+            {
+                AmbientColor = Color.Gray,
+                DiffuseColor = Color.White,
+                SpecularColor = Color.White,
+                SpecularShininess = 100f,
+            };
+
+            this.InnerModelMaterial = new PhongMaterial
+            {
+                AmbientColor = Color.Gray,
+                DiffuseColor = new Color4(0.75f, 0.75f, 0.75f, 1.0f),
+                SpecularColor = Color.White,
+                SpecularShininess = 100f
+            };
+
+            //this.OtherMaterial = new PhongMaterial
+            //{
+            //    AmbientColor = Color.Gray,
+            //    DiffuseColor = new Color4(0.75f, 0.75f, 0.75f, 1.0f),
+            //    SpecularColor = Color.White,
+            //    SpecularShininess = 100f,
+            //    DiffuseMap = ModelMaterial.DiffuseMap,
+            //    NormalMap = ModelMaterial.NormalMap
+            //};
+
+            initialPosition = Model.Positions;
+            initialIndicies = Model.Indices;
+
+            //var b3 = new MeshBuilder(true, true, true);
+            //b3.AddBox(new Vector3(3, 3, 3), 1, 2,  2);
+            //Other = b3.ToMeshGeometry3D();
+
+            this.PropertyChanged += MainViewModel_PropertyChanged;
+
+            LineColor = Color.Blue;
+
+            var lineBuilder = new LineBuilder();
+            GeometryOctree octree = new GeometryOctree(Model.Positions, Model.Indices);
+            octree.UpdateTree();
+            CreateOctreeSegments(lineBuilder, octree);
+            OctreeModel = lineBuilder.ToLineGeometry3D();
+        }
+
+        private void CreateOctreeSegments(LineBuilder builder, GeometryOctree tree)
+        {
+            if (tree == null) return;
+            var box = tree.Region;
+            Vector3[] verts = new Vector3[8];
+            verts[0] = box.Minimum;
+            verts[1] = new Vector3(box.Minimum.X, box.Minimum.Y, box.Maximum.Z); //Z
+            verts[2] = new Vector3(box.Minimum.X, box.Maximum.Y, box.Minimum.Z); //Y
+            verts[3] = new Vector3(box.Maximum.X, box.Minimum.Y, box.Minimum.Z); //X
+
+            verts[7] = box.Maximum;
+            verts[4] = new Vector3(box.Maximum.X, box.Maximum.Y, box.Minimum.Z); //Z
+            verts[5] = new Vector3(box.Maximum.X, box.Minimum.Y, box.Maximum.Z); //Y
+            verts[6] = new Vector3(box.Minimum.X, box.Maximum.Y, box.Maximum.Z); //X
+            builder.AddLine(verts[0], verts[1]);
+            builder.AddLine(verts[0], verts[2]);
+            builder.AddLine(verts[0], verts[3]);
+            builder.AddLine(verts[7], verts[4]);
+            builder.AddLine(verts[7], verts[5]);
+            builder.AddLine(verts[7], verts[6]);
+
+            builder.AddLine(verts[1], verts[6]);
+            builder.AddLine(verts[1], verts[5]);
+            builder.AddLine(verts[4], verts[2]);
+            builder.AddLine(verts[4], verts[3]);
+            builder.AddLine(verts[2], verts[6]);
+            builder.AddLine(verts[3], verts[5]);
+
+            if (tree.HasChildren)
+            {
+                foreach(var child in tree.ChildNodes)
+                {
+                    CreateOctreeSegments(builder, child as GeometryOctree);
+                }
+            }          
+        }
+
+
+        private void MainViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals(nameof(CamLookDir)))
+            {
+                Light1Direction = CamLookDir.ToVector3();
+            }
+        }
+
+        public void SetupCameraBindings(Camera camera)
+        {
+            if (camera is ProjectionCamera)
+            {
+                SetBinding("CamLookDir", camera, ProjectionCamera.LookDirectionProperty, this);
+            }
+        }
+
+        private static void SetBinding(string path, DependencyObject dobj, DependencyProperty property, object viewModel, BindingMode mode = BindingMode.TwoWay)
+        {
+            var binding = new Binding(path);
+            binding.Source = viewModel;
+            binding.Mode = mode;
+            BindingOperations.SetBinding(dobj, property, binding);
+        }
+
+    }
+}
---
+++ Source/HelixToolkit.SharpDX.Shared/Utilities/Octree.cs
@@ -0,0 +1,339 @@
+﻿using HelixToolkit.Wpf.SharpDX.Core;
+using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelixToolkit.SharpDX.Shared.Utilities
+{
+    public abstract class OctreeBase<T>
+    {
+        /// <summary>
+        /// The minumum size for enclosing region is a 1x1x1 cube.
+        /// </summary>
+        public const int MIN_SIZE = 1;
+
+        public BoundingBox Region { protected set; get; }
+
+        public List<T> Objects { protected set; get; }
+
+        /// <summary>
+        /// These are all of the possible child octants for this node in the tree.
+        /// </summary>
+        public readonly OctreeBase<T>[] ChildNodes = new OctreeBase<T>[8];
+
+        /// <summary>
+        /// This is a bitmask indicating which child nodes are actively being used.
+        /// It adds slightly more complexity, but is faster for performance since there is only one comparison instead of 8.
+        /// </summary>
+        public byte ActiveNodes { protected set; get; }
+
+        /// <summary>
+        /// A reference to the parent node is sometimes required. If we are a node and we realize that we no longer have items contained within ourselves,
+        /// we need to let our parent know that we're empty so that it can delete us.
+        /// </summary>
+        public OctreeBase<T> Parent { protected set; get; }
+
+        protected bool treeReady = false;       //the tree has a few objects which need to be inserted before it is complete
+        protected bool treeBuilt = false;       //there is no pre-existing tree yet.
+
+
+        /*Note: we want to avoid allocating memory for as long as possible since there can be lots of nodes.*/
+        /// <summary>
+        /// Creates an oct tree which encloses the given region and contains the provided objects.
+        /// </summary>
+        /// <param name="region">The bounding region for the oct tree.</param>
+        /// <param name="objList">The list of objects contained within the bounding region</param>
+        protected OctreeBase(BoundingBox region, List<T> objList)
+        {
+            Region = region;
+            Objects = objList;
+        }
+
+        public OctreeBase()
+        {
+            Objects = new List<T>();
+            Region = new BoundingBox(Vector3.Zero, Vector3.Zero);
+        }
+
+        /// <summary>
+        /// Creates an octTree with a suggestion for the bounding region containing the items.
+        /// </summary>
+        /// <param name="region">The suggested dimensions for the bounding region. 
+        /// Note: if items are outside this region, the region will be automatically resized.</param>
+        public OctreeBase(BoundingBox region)
+            :this()
+        {
+            Region = region;
+        }
+
+        /// <summary>
+        /// Naively builds an oct tree from scratch.
+        /// </summary>
+        protected abstract void BuildTree();    //complete & tested
+
+        protected abstract OctreeBase<T> CreateNode(BoundingBox region, List<T> objList);  //complete & tested
+
+
+        protected OctreeBase<T> CreateNode(BoundingBox region, T Item)
+        {
+            return CreateNode(region, new List<T> { Item });
+        }
+
+        /// <summary>
+        /// Processes all pending insertions by inserting them into the tree.
+        /// </summary>
+        /// <remarks>Consider deprecating this?</remarks>
+        public void UpdateTree()   //complete & tested
+        {
+            /*I think I can just directly insert items into the tree instead of using a queue.*/
+            if (!treeBuilt)
+            {
+                BuildTree();
+            }
+            treeReady = true;
+        }
+
+        /// <summary>
+        /// This finds the dimensions of the bounding box necessary to tightly enclose all items in the object list.
+        /// </summary>
+        protected abstract void FindEnclosingBox();
+
+        /// <summary>
+        /// This finds the smallest enclosing cube which is a power of 2, for all objects in the list.
+        /// </summary>
+        protected abstract void FindEnclosingCube();
+
+
+
+        #region Accessors
+        public bool IsRoot
+        {
+            //The root node is the only node without a parent.
+            get { return Parent == null; }
+        }
+
+        public bool HasChildren
+        {
+            get
+            {
+                return ActiveNodes != 0;
+            }
+        }
+
+        /// <summary>
+        /// Returns true if this node tree and all children have no content
+        /// </summary>
+        private bool IsEmpty    //untested
+        {
+            get
+            {
+                if (Objects.Count != 0)
+                    return false;
+                else
+                {
+                    for (int a = 0; a < 8; a++)
+                    {
+                        //note that we have to do this recursively. 
+                        //Just checking child nodes for the current node doesn't mean that their children won't have objects.
+                        if (ChildNodes[a] != null && !ChildNodes[a].IsEmpty)
+                            return false;
+                    }
+
+                    return true;
+                }
+            }
+        }
+        #endregion
+    }
+
+
+    public class GeometryOctree
+        : OctreeBase<int>
+    {
+        public IList<Vector3> Positions { private set; get; }
+        public IList<int> Indices { private set; get; }
+        public GeometryOctree(Vector3Collection positions, IList<int> indices)
+            : base(BoundingBox.FromPoints(positions.Array), Enumerable.Range(0, indices.Count/3).ToList())
+        {
+            Positions = positions;
+            Indices = indices;
+        }
+
+        public GeometryOctree(IList<Vector3> positions, IList<int> indices, BoundingBox box, List<int> triIndex)
+            : base(box, triIndex)
+        {
+            Positions = positions;
+            Indices = indices;
+        }
+
+        protected GeometryOctree(BoundingBox box, List<int> list):base(box, list) { }
+
+        private BoundingBox GetBoundingBox(int triangleIndex)
+        {
+            var actual = triangleIndex * 3;
+            var v1 = Positions[Indices[actual++]];
+            var v2 = Positions[Indices[actual++]];
+            var v3 = Positions[Indices[actual++]];
+            var maxX = Math.Max(v1.X, Math.Max(v2.X, v3.X));
+            var maxY = Math.Max(v1.Y, Math.Max(v2.Y, v3.Y));
+            var maxZ = Math.Max(v1.Z, Math.Max(v2.Z, v3.Z));
+
+            var minX = Math.Min(v1.X, Math.Min(v2.X, v3.X));
+            var minY = Math.Min(v1.Y, Math.Min(v2.Y, v3.Y));
+            var minZ = Math.Min(v1.Z, Math.Min(v2.Z, v3.Z));
+
+            return new BoundingBox(new Vector3(minX, minY, minZ), new Vector3(maxX, maxY, maxZ));
+        }
+        protected override void BuildTree()
+        {
+            //terminate the recursion if we're a leaf node
+            if (Objects.Count <= 1)   //doubt: is this really right? needs testing.
+                return;
+
+            Vector3 dimensions = Region.Maximum - Region.Minimum;
+
+            if (dimensions == Vector3.Zero)
+            {
+                FindEnclosingCube();
+                dimensions = Region.Maximum - Region.Minimum;
+            }
+
+            //Check to see if the dimensions of the box are greater than the minimum dimensions
+            if (dimensions.X <= MIN_SIZE && dimensions.Y <= MIN_SIZE && dimensions.Z <= MIN_SIZE)
+            {
+                return;
+            }
+
+            Vector3 half = dimensions / 2.0f;
+            Vector3 center = Region.Minimum + half;
+
+            //Create subdivided regions for each octant
+            BoundingBox[] octant = new BoundingBox[8];
+            octant[0] = new BoundingBox(Region.Minimum, center);
+            octant[1] = new BoundingBox(new Vector3(center.X, Region.Minimum.Y, Region.Minimum.Z), new Vector3(Region.Maximum.X, center.Y, center.Z));
+            octant[2] = new BoundingBox(new Vector3(center.X, Region.Minimum.Y, center.Z), new Vector3(Region.Maximum.X, center.Y, Region.Maximum.Z));
+            octant[3] = new BoundingBox(new Vector3(Region.Minimum.X, Region.Minimum.Y, center.Z), new Vector3(center.X, center.Y, Region.Maximum.Z));
+            octant[4] = new BoundingBox(new Vector3(Region.Minimum.X, center.Y, Region.Minimum.Z), new Vector3(center.X, Region.Maximum.Y, center.Z));
+            octant[5] = new BoundingBox(new Vector3(center.X, center.Y, Region.Minimum.Z), new Vector3(Region.Maximum.X, Region.Maximum.Y, center.Z));
+            octant[6] = new BoundingBox(center, Region.Maximum);
+            octant[7] = new BoundingBox(new Vector3(Region.Minimum.X, center.Y, center.Z), new Vector3(center.X, Region.Maximum.Y, Region.Maximum.Z));
+
+            //This will contain all of our objects which fit within each respective octant.
+            List<int>[] octList = new List<int>[8];
+            for (int i = 0; i < 8; i++) octList[i] = new List<int>();
+
+            //this list contains all of the objects which got moved down the tree and can be delisted from this node.
+            List<int> delist = new List<int>();
+
+            foreach (int obj in Objects)
+            {
+                var box = GetBoundingBox(obj);
+                if (box.Minimum != box.Maximum)
+                {
+                    for (int i = 0; i < 8; i++)
+                    {
+                        if (octant[i].Contains(box) == ContainmentType.Contains)
+                        {
+                            octList[i].Add(obj);
+                            delist.Add(obj);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            //delist every moved object from this node.
+            foreach (int obj in delist)
+                Objects.Remove(obj);
+
+            //Create child nodes where there are items contained in the bounding region
+            for (int i = 0; i < 8; i++)
+            {
+                if (octList[i].Count != 0)
+                {
+                    ChildNodes[i] = CreateNode(octant[i], octList[i]);
+                    ActiveNodes |= (byte)(1 << i);
+                    (ChildNodes[i] as GeometryOctree).BuildTree();
+                }
+            }
+
+            treeBuilt = true;
+            treeReady = true;
+        }
+
+        protected override OctreeBase<int> CreateNode(BoundingBox region, List<int> objList)
+        {
+            return new GeometryOctree(Positions, Indices, region, objList);
+        }
+
+        protected override void FindEnclosingBox()
+        {
+            Vector3 global_min = Region.Minimum, global_max = Region.Maximum;
+
+            //go through all the objects in the list and find the extremes for their bounding areas.
+            foreach (var obj in Objects)
+            {
+                Vector3 local_min = Vector3.Zero, local_max = Vector3.Zero;
+                var BoundingBox = GetBoundingBox(obj);
+                if (BoundingBox != null && BoundingBox.Maximum != BoundingBox.Minimum)
+                {
+                    local_min = BoundingBox.Minimum;
+                    local_max = BoundingBox.Maximum;
+                }
+
+                if (local_min.X < global_min.X) global_min.X = local_min.X;
+                if (local_min.Y < global_min.Y) global_min.Y = local_min.Y;
+                if (local_min.Z < global_min.Z) global_min.Z = local_min.Z;
+
+                if (local_max.X > global_max.X) global_max.X = local_max.X;
+                if (local_max.Y > global_max.Y) global_max.Y = local_max.Y;
+                if (local_max.Z > global_max.Z) global_max.Z = local_max.Z;
+            }
+
+            Region = new BoundingBox(global_min, global_max);
+        }
+
+        protected override void FindEnclosingCube()
+        {
+            FindEnclosingBox();
+
+            //find the min offset from (0,0,0) and translate by it for a short while
+            Vector3 offset = Region.Minimum - Vector3.Zero;
+            Region = new BoundingBox(Region.Minimum + offset, Region.Maximum + offset);
+
+            //find the nearest power of two for the max values
+            int highX = (int)Math.Floor(Math.Max(Math.Max(Region.Maximum.X, Region.Maximum.Y), Region.Maximum.Z));
+
+            //see if we're already at a power of 2
+            for (int bit = 0; bit < 32; bit++)
+            {
+                if (highX == 1 << bit)
+                {
+                    Region = new BoundingBox(Region.Minimum - offset, new Vector3(highX, highX, highX) - offset);
+                    return;
+                }
+            }
+
+            //gets the most significant bit value, so that we essentially do a Ceiling(X) with the 
+            //ceiling result being to the nearest power of 2 rather than the nearest integer.
+            int x = SigBit(highX);
+
+            Region = new BoundingBox(Region.Minimum - offset, new Vector3(x, x, x) - offset);
+        }
+
+        private static int SigBit(int x)
+        {
+            if (x >= 0)
+            {
+                return (int)Math.Pow(2, Math.Ceiling(Math.Log(x) / Math.Log(2)));
+            }
+            else
+            {
+                x = Math.Abs(x);
+                return -(int)Math.Pow(2, Math.Ceiling(Math.Log(x) / Math.Log(2)));
+            }
+        }
+    }
+}
