abed0472279a613870e0f60aa9d16efde654636b
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2017-06-22 15:00:27 +0200

    Add tests for issue #271

--- Source/HelixToolkit.Wpf.SharpDX.Tests/Utilities/MeshBuilderTests.cs
+++ Source/HelixToolkit.Wpf.SharpDX.Tests/Utilities/MeshBuilderTests.cs
@@ -32,5 +32,42 @@ namespace HelixToolkit.Wpf.SharpDX.Tests.Utilities
             Assert.IsTrue(builder.HasNormals);
             Assert.AreEqual(3, builder.Normals.Count);
         }
+
+        [Test]
+        public void AddTriangle_Normals()
+        {
+            var mb = new MeshBuilder();
+            var p0 = new Vector3(0, 0, 0);
+            var p1 = new Vector3(1, 0, 0);
+            var p2 = new Vector3(1, 1, 0);
+            mb.AddTriangle(p0, p1, p2);
+
+            Assert.IsTrue(mb.HasNormals);
+            Assert.AreEqual(3, mb.Normals.Count);
+
+            foreach (Vector3 normal in mb.Normals)
+            {
+                Assert.AreEqual(new Vector3(0, 0, 1), normal);
+            }
+        }
+
+        [Test]
+        public void AddQuad_Normals()
+        {
+            var mb = new MeshBuilder();
+            var p0 = new Vector3(0, 0, 0);
+            var p1 = new Vector3(1, 0, 0);
+            var p2 = new Vector3(1, 1, 0);
+            var p3 = new Vector3(0, 1, 0);
+            mb.AddQuad(p0, p1, p2, p3);
+
+            Assert.IsTrue(mb.HasNormals);
+            Assert.AreEqual(4, mb.Normals.Count);
+
+            foreach (Vector3 normal in mb.Normals)
+            {
+                Assert.AreEqual(new Vector3(0, 0, 1), normal);
+            }
+        }
     }
 }
