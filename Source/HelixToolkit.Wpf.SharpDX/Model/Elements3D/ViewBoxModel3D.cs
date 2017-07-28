8079c0712a9c7258fea861b52f1e15c506e6a8bd
Lunci <lunci.app@gmail.com>
2017-07-27 19:53:56 -0700

    Update ViewBoxModel3D

--- Source/HelixToolkit.Wpf.SharpDX/Model/Elements3D/ViewBoxModel3D.cs
+++ Source/HelixToolkit.Wpf.SharpDX/Model/Elements3D/ViewBoxModel3D.cs
@@ -8,6 +8,7 @@ using SharpDX.Direct3D;
 using SharpDX.Direct3D11;
 using SharpDX.DXGI;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -25,7 +26,32 @@ namespace HelixToolkit.Wpf.SharpDX
             builder.AddBox(Vector3.Zero, 10, 10, 10);
             var mesh = builder.ToMesh();
             CreateTextureCoordinates(mesh);
-            defaultBoxModel = mesh;
+
+
+            var pts = new List<Vector3>();
+            var up = new Vector3(1, 0, 0);
+            var normal = new Vector3(0, 1, 0);
+            var right = Vector3.Cross(up, normal);
+            var center = new Vector3(0, -7, 0);
+            for (int i = 0; i < 20; i++)
+            {
+                double angle = 0 + (360 * i / (20 - 1));
+                double angleRad = angle / 180 * Math.PI;
+                var dir = (right * (float)Math.Cos(angleRad)) + (up * (float)Math.Sin(angleRad));
+                pts.Add(center + (dir * 8));
+                pts.Add(center + (dir * 12));
+            }           
+            builder = new MeshBuilder(false, false, false);
+            builder.AddTriangleStrip(pts);
+            var pie = builder.ToMesh();
+            pie.Normals = pie.CalculateNormals();
+
+            var newMesh = MeshGeometry3D.Merge(new MeshGeometry3D[] { pie, mesh});
+
+            newMesh.TextureCoordinates = new Core.Vector2Collection(Enumerable.Repeat(new Vector2(0, 0), pie.Positions.Count));
+            newMesh.TextureCoordinates.AddRange(mesh.TextureCoordinates);
+
+            defaultBoxModel = newMesh;
         }
         public ViewBoxModel3D()
         {
@@ -46,7 +72,6 @@ namespace HelixToolkit.Wpf.SharpDX
         {
             return host.EffectsManager.RenderTechniquesManager.RenderTechniques[DefaultRenderTechniqueNames.Diffuse];
         }
-
 
         private static void CreateTextureCoordinates(MeshGeometry3D mesh)
         {
