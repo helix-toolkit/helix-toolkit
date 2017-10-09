9fcdc72802ce8feb267c7fbd596bcf7d3c48145d
Lunci <lunci.app@gmail.com>
2017-10-08 22:34:16 -0700

    Update assembly version

--- Source/AssemblyInfo.cs
+++ Source/AssemblyInfo.cs
@@ -9,9 +9,9 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Helix Toolkit")]
 [assembly: AssemblyCompany("Helix Toolkit")]
-[assembly: AssemblyCopyright("Copyright (C) Helix Toolkit 2010-2014.")]
+[assembly: AssemblyCopyright("Copyright (C) Helix Toolkit 2010-2017.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2014.2.1.1")]
-[assembly: AssemblyFileVersion("2014.2.1.1")]
+[assembly: AssemblyVersion("2017.10.9.1")]
+[assembly: AssemblyFileVersion("2017.10.9.1")]
