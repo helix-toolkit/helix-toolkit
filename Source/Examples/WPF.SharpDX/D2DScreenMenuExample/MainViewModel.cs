4ebee363d19718399bd52e9f7b0f2e5232ea6b9a
Lunci <lunci.app@gmail.com>
2017-10-20 17:01:50 -0700

    Demo update

--- Source/Examples/WPF.SharpDX/D2DScreenMenuExample/MainViewModel.cs
+++ Source/Examples/WPF.SharpDX/D2DScreenMenuExample/MainViewModel.cs
@@ -3,6 +3,7 @@ using HelixToolkit.Wpf.SharpDX;
 using SharpDX;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,18 +35,44 @@ namespace D2DScreenMenuExample
     public class ViewModel3D : ObservableObject
     {
         public MeshGeometry3D Model { set; get; }
-        public Material ModelMaterial { set; get; } = PhongMaterials.Gray;
+        public PhongMaterial ModelMaterial { set; get; } = PhongMaterials.White;
 
-        public Vector3 Light1Direction { get; set; } = new Vector3(-1, -1, -1);
+        public Vector3 Light1Direction { get; set; } = new Vector3(1, -1, -1);
 
-        public Color4 Light1Color { set; get; } = Color.White;
+        public Color4 Light1Color { set; get; } = Color.Blue;
+
+        public Vector3 Light2Direction { get; set; } = new Vector3(-1, -1, -1);
+
+        public Color4 Light2Color { set; get; } = Color.Red;
+
+        public Vector3 Light3Direction { get; set; } = new Vector3(-1, -1, 1);
+
+        public Color4 Light3Color { set; get; } = Color.Green;
+
+        private string NormalTexture = @"TextureCheckerboard2_dot3.jpg";
+
+        private string Texture = @"TextureCheckerboard2.jpg";
 
         public ViewModel3D()
         {
-            var builder = new MeshBuilder(true);
-            builder.AddBox(new Vector3(0, 0, 0), 5, 5, 5);
+            var builder = new MeshBuilder(true, true, true);
+            builder.AddBox(new Vector3(0, 2.5f, 0), 5, 5, 5);
             builder.AddBox(new Vector3(0, 0, 0), 10, 0.1, 10);
             Model = builder.ToMeshGeometry3D();
+            var diffuseMap = LoadFileToMemory(new System.Uri(Texture, System.UriKind.RelativeOrAbsolute).ToString());
+            var normalMap = LoadFileToMemory(new System.Uri(NormalTexture, System.UriKind.RelativeOrAbsolute).ToString());
+            ModelMaterial.DiffuseMap = diffuseMap;
+            ModelMaterial.NormalMap = normalMap;
+        }
+
+        public static MemoryStream LoadFileToMemory(string filePath)
+        {
+            using (var file = new FileStream(filePath, FileMode.Open))
+            {
+                var memory = new MemoryStream();
+                file.CopyTo(memory);
+                return memory;
+            }
         }
     }
 }
