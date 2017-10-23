54c1034ac4109b693bd6c9000e86ff44c8ed9ab4
Lunci <lunci.app@gmail.com>
2017-10-23 10:48:39 -0700

    Cache GetScreenViewProjectionMatrix

--- Source/HelixToolkit.Wpf.SharpDX/Controls/RenderContext.cs
+++ Source/HelixToolkit.Wpf.SharpDX/Controls/RenderContext.cs
@@ -29,13 +29,46 @@ namespace HelixToolkit.Wpf.SharpDX
         private Camera camera; 
         private EffectVectorVariable vEyePos, vFrustum, vViewport;        
         private EffectMatrixVariable mView, mProjection;
+        private bool matrixChanged = true;
 
+        public Matrix ViewMatrix
+        {
+            get { return this.viewMatrix; }
+            private set
+            {
+                if (viewMatrix == value) { return; }
+                viewMatrix = value;
+                matrixChanged = true;
+            }
+        }
 
-        public Matrix ViewMatrix { get { return this.viewMatrix; } }
+        public Matrix ProjectionMatrix
+        {
+            get { return this.projectionMatrix; }
+            set
+            {
+                if (projectionMatrix == value)
+                {
+                    return;
+                }
+                projectionMatrix = value;
+                matrixChanged = true;
+            }
+        }
 
-        public Matrix ProjectionMatrix { get { return this.projectionMatrix; } }
-
-        public Matrix WorldMatrix { get { return worldMatrix; } }
+        public Matrix WorldMatrix
+        {
+            get { return worldMatrix; }
+            set
+            {
+                if (worldMatrix == value)
+                {
+                    return;
+                }
+                worldMatrix = value;
+                matrixChanged = true;
+            }
+        }
 
         public Matrix ViewportMatrix
         {
@@ -48,6 +81,7 @@ namespace HelixToolkit.Wpf.SharpDX
             }
         }
 
+        private Matrix screenViewProjectionMatrix = Matrix.Identity;
         public Matrix ScreenViewProjectionMatrix
         {
             get
@@ -127,7 +161,12 @@ namespace HelixToolkit.Wpf.SharpDX
 
         public Matrix GetScreenViewProjectionMatrix()
         {
-            return viewMatrix * projectionMatrix * ViewportMatrix;
+            if (matrixChanged)
+            {
+                screenViewProjectionMatrix = viewMatrix * projectionMatrix * ViewportMatrix;
+                matrixChanged = false;
+            }
+            return screenViewProjectionMatrix;
         }
 
         ~RenderContext()
