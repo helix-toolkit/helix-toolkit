91cef69de573188a0767266a15f5d76fe1503c6b
Lunci <lunci.app@gmail.com>
2017-11-17 17:10:41 -0800

    Add comments

--- Source/HelixToolkit.SharpDX.Shared/Interface/IRenderCore.cs
+++ Source/HelixToolkit.SharpDX.Shared/Interface/IRenderCore.cs
@@ -10,10 +10,24 @@ namespace HelixToolkit.UWP
 {
     public interface IRenderCore : IGUID
     {
+        /// <summary>
+        /// If render core is attached or not
+        /// </summary>
+        bool IsAttached { get; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="technique"></param>
         void Attach(IRenderHost host, RenderTechnique technique);
-
+        /// <summary>
+        /// 
+        /// </summary>
         void Detach();
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
         void Render(IRenderMatrices context);
     }
 }
