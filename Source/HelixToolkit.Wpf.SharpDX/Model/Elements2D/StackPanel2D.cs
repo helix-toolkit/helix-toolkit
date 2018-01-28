a67361c6454327b63c0a216822455aa91bef5363
Lunci <lunci.app@gmail.com>
2018-01-27 20:16:29 -0800

    Add stack panel

---
+++ Source/HelixToolkit.Wpf.SharpDX/Model/Elements2D/StackPanel2D.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using HelixToolkit.Wpf.SharpDX.Core2D;
+using SharpDX;
+
+namespace HelixToolkit.Wpf.SharpDX.Elements2D
+{
+    public class StackPanel2D : Panel2D
+    {
+        /// <summary>
+        /// Gets or sets the orientation.
+        /// </summary>
+        /// <value>
+        /// The orientation.
+        /// </value>
+        public Orientation Orientation
+        {
+            get { return (Orientation)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
+
+        /// <summary>
+        /// The orientation property
+        /// </summary>
+        public static readonly DependencyProperty OrientationProperty =
+            DependencyProperty.Register("Orientation", typeof(Orientation), typeof(StackPanel2D), new FrameworkPropertyMetadata(Orientation.Horizontal, FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+
+        protected override Size2F MeasureOverride(Size2F availableSize)
+        {
+            var constraint = availableSize;
+
+            var size = new Size2F();
+            switch (Orientation)
+            {
+                case Orientation.Horizontal:
+                    availableSize.Width = float.PositiveInfinity;
+                    break;
+                case Orientation.Vertical:
+                    availableSize.Height = float.PositiveInfinity;
+                    break;
+            }
+
+            foreach(var child in Items)
+            {
+                if(child is Element2DCore c)
+                {
+                    child.Measure(availableSize);
+                    switch (Orientation)
+                    {
+                        case Orientation.Horizontal:
+                            size.Width += c.DesiredSize.X;
+                            size.Height = Math.Max(size.Height, c.DesiredSize.Y);
+                            break;
+                        case Orientation.Vertical:
+                            size.Width = Math.Max(c.DesiredSize.X, size.Width);
+                            size.Height += c.DesiredSize.Y;
+                            break;
+                    }
+                }
+            }
+
+            return size;
+        }
+
+        protected override RectangleF ArrangeOverride(RectangleF finalSize)
+        {
+            float lastSize = 0;
+            var totalSize = new RectangleF() { Left = finalSize.Left, Top = finalSize.Top };
+            foreach(var child in Items)
+            {
+                if(child is Element2DCore c)
+                {
+                    switch (Orientation)
+                    {
+                        case Orientation.Horizontal:
+                            finalSize.Left += lastSize;
+                            lastSize = c.DesiredSize.X;
+                            finalSize.Right = finalSize.Left + lastSize;
+                            finalSize.Bottom = finalSize.Top + Math.Min(finalSize.Height, c.DesiredSize.Y);
+                            totalSize.Width += lastSize;
+                            totalSize.Bottom = finalSize.Bottom;
+                            break;
+                        case Orientation.Vertical:
+                            finalSize.Top += lastSize;
+                            lastSize = c.DesiredSize.Y;
+                            finalSize.Right = finalSize.Left + Math.Min(finalSize.Width, c.DesiredSize.X);
+                            finalSize.Bottom = finalSize.Top + lastSize;
+                            totalSize.Bottom += lastSize;
+                            totalSize.Right = finalSize.Right;
+                            break;
+                    }
+                    c.Arrange(finalSize);
+                }
+            }
+            
+            return totalSize;
+        }
+    }
+}
