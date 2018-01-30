06160b76a6f4c0ec720a7585808720829c3544d2
Lunci <lunci.app@gmail.com>
2018-01-30 01:12:52 -0800

    Fix panel2D hit test

--- Source/HelixToolkit.Wpf.SharpDX/Model/Elements2D/Panel2D.cs
+++ Source/HelixToolkit.Wpf.SharpDX/Model/Elements2D/Panel2D.cs
@@ -103,9 +103,14 @@ namespace HelixToolkit.Wpf.SharpDX.Elements2D
             base.OnDetach();
         }
 
+
         protected override bool OnHitTest(ref Vector2 mousePoint, out HitTest2DResult hitResult)
         {
             hitResult = null;
+            if (!LayoutBoundWithTransform.Contains(mousePoint))
+            {
+                return false;
+            }
             foreach (var item in Items.Reverse())
             {
                 if (item is IHitable2D && (item as IHitable2D).HitTest(mousePoint, out hitResult))
@@ -113,15 +118,7 @@ namespace HelixToolkit.Wpf.SharpDX.Elements2D
                     return true;
                 }
             }
-            if (LayoutBoundWithTransform.Contains(mousePoint))
-            {
-                hitResult = new HitTest2DResult(this);
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return false;
         }
     }
 }
--- Source/HelixToolkit.Wpf.SharpDX/Model/Elements2D/StackPanel2D.cs
+++ Source/HelixToolkit.Wpf.SharpDX/Model/Elements2D/StackPanel2D.cs
@@ -98,22 +98,5 @@ namespace HelixToolkit.Wpf.SharpDX.Elements2D
             
             return finalSize;
         }
-
-        protected override bool OnHitTest(ref Vector2 mousePoint, out HitTest2DResult hitResult)
-        {
-            hitResult = null;
-            if (!LayoutBoundWithTransform.Contains(mousePoint))
-            {
-                return false;
-            }
-            foreach (var item in Items.Reverse())
-            {
-                if (item is IHitable2D && (item as IHitable2D).HitTest(mousePoint, out hitResult))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
     }
 }
