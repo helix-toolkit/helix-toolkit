93d1ee56c9e534cfca448b2e826de5d91f5d8e99
holance <lunci.app@gmail.com>
2018-02-09 11:28:29 -0800

    Fix comment

--- Source/HelixToolkit/Logger/ILogger.cs
+++ Source/HelixToolkit/Logger/ILogger.cs
@@ -14,9 +14,9 @@ namespace HelixToolkit.Logger
         /// <summary>
         /// Logs the specified log level.
         /// </summary>
-        /// <typeparam name="TState">The type of the state.</typeparam>
+        /// <typeparam name="MsgType">The type of the sg type.</typeparam>
         /// <param name="logLevel">The log level.</param>
-        /// <param name="msg">The message.</param>
+        /// <param name="msg">The MSG.</param>
         /// <param name="caller">The caller.</param>
         void Log<MsgType>(LogLevel logLevel, MsgType msg, string caller);
     }
