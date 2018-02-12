b3c8afc5b2d40ef5d83068856196c844e03db941
holance <lunci.app@gmail.com>
2018-02-12 10:19:28 -0800

    Fix instancing bound

--- Source/HelixToolkit.SharpDX.Shared/Model/Element3D/GeometryBoundManager.cs
+++ Source/HelixToolkit.SharpDX.Shared/Model/Element3D/GeometryBoundManager.cs
@@ -224,10 +224,10 @@ namespace HelixToolkit.Wpf.SharpDX.Core
             }
             else
             {
-                Bounds = Geometry.Bound;
-                BoundsSphere = Geometry.BoundingSphere;
                 if (!HasInstances)
                 {
+                    Bounds = Geometry.Bound;
+                    BoundsSphere = Geometry.BoundingSphere;
                     BoundsWithTransform = Bounds.Transform(elementCore.ModelMatrix);
                     BoundsSphereWithTransform = BoundsSphere.TransformBoundingSphere(elementCore.ModelMatrix);
                 }
@@ -264,6 +264,8 @@ namespace HelixToolkit.Wpf.SharpDX.Core
                             BoundingSphere.Merge(ref boundSphere, ref bs, out boundSphere);
                         }
                     }
+                    Bounds = bound;
+                    BoundsSphere = boundSphere;
                     BoundsWithTransform = Bounds.Transform(elementCore.ModelMatrix);
                     BoundsSphereWithTransform = BoundsSphere.TransformBoundingSphere(elementCore.ModelMatrix);
                 }
