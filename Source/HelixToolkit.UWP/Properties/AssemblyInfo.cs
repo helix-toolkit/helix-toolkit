2871c6a3f12e064cb061a2e696cdd805accf2b6f
holance <lunci.app@gmail.com>
2018-04-04 13:24:36 -0700

    Add uwp nuget

--- Source/HelixToolkit.UWP/Properties/AssemblyInfo.cs
+++ Source/HelixToolkit.UWP/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HelixToolkit.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
