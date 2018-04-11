fa4e88c8558cc88bb812ec03f55f4869e84d9115
Lunci <lunci.app@gmail.com>
2018-04-10 23:25:52 -0700

    Implement ItemsSource for GroupElement3D in UWP

--- Source/HelixToolkit.UWP/Model/Element3D/Abstract/GroupElement3D.cs
+++ Source/HelixToolkit.UWP/Model/Element3D/Abstract/GroupElement3D.cs
@@ -7,10 +7,12 @@ using System.Collections;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using Windows.UI.Xaml.Markup;
-
+using System.Collections.Generic;
+using Windows.UI.Xaml;
 namespace HelixToolkit.UWP
 {
     using Model.Scene;
+   
 
     /// <summary>
     /// 
@@ -19,6 +21,24 @@ namespace HelixToolkit.UWP
     [ContentProperty(Name = "Children")]
     public abstract class GroupElement3D : Element3D
     {
+        private IList<Element3D> itemsSourceInternal;
+        /// <summary>
+        /// ItemsSource for binding to collection. Please use ObservableElement3DCollection for observable, otherwise may cause memory leak.
+        /// </summary>
+        public IList<Element3D> ItemsSource
+        {
+            get { return (IList<Element3D>)this.GetValue(ItemsSourceProperty); }
+            set { this.SetValue(ItemsSourceProperty, value); }
+        }
+        /// <summary>
+        /// ItemsSource for binding to collection. Please use ObservableElement3DCollection for observable, otherwise may cause memory leak.
+        /// </summary>
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSource", typeof(IList<Element3D>), typeof(GroupElement3D),
+                new PropertyMetadata(null,
+                    (d, e) => {
+                        (d as GroupElement3D).OnItemsSourceChanged(e.NewValue as IList<Element3D>);
+                    }));
         /// <summary>
         /// Gets the children.
         /// </summary>
@@ -108,6 +128,51 @@ namespace HelixToolkit.UWP
                 if(node.RemoveChildNode(c) && itemsContainer != null)
                 {
                     itemsContainer.Items.Remove(c);
+                }
+            }
+        }
+
+        private void OnItemsSourceChanged(IList<Element3D> itemsSource)
+        {
+            if (itemsSourceInternal != null)
+            {
+                if (itemsSourceInternal is INotifyCollectionChanged s)
+                {
+                    s.CollectionChanged -= S_CollectionChanged;
+                }
+                foreach (var child in itemsSourceInternal)
+                {
+                    Children.Remove(child);
+                }
+            }
+            itemsSourceInternal = itemsSource;
+            if (itemsSourceInternal != null)
+            {
+                if (itemsSourceInternal is INotifyCollectionChanged s)
+                {
+                    s.CollectionChanged += S_CollectionChanged;
+                }
+                foreach (var child in itemsSourceInternal)
+                {
+                    Children.Add(child);
+                }
+            }
+        }
+
+        private void S_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.OldItems != null)
+            {
+                foreach (Element3D item in e.OldItems)
+                {
+                    Children.Remove(item);
+                }
+            }
+            if (e.NewItems != null)
+            {
+                foreach (Element3D item in e.NewItems)
+                {
+                    Children.Add(item);
                 }
             }
         }
