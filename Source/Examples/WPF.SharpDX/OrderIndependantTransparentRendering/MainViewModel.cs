b76e6f5a48205f59d9bed2e67b611241df0c49cf
Lunci <lunci.app@gmail.com>
2018-04-20 19:08:57 -0700

    Improve OIT weighting equation. Update demo.

--- Source/Examples/WPF.SharpDX/OrderIndependantTransparentRendering/MainViewModel.cs
+++ Source/Examples/WPF.SharpDX/OrderIndependantTransparentRendering/MainViewModel.cs
@@ -19,18 +19,21 @@ namespace OrderIndependentTransparentRendering
 
         public ObservableElement3DCollection ModelGeometry { get; private set; }
 
+        public ObservableElement3DCollection PlaneGeometry { private set; get; }
+
         public LineGeometry3D GridModel { private set; get; }
 
         public Media3D.Transform3D GridTransform { private set; get; }
 
         private bool showWireframe = false;
+
         public bool ShowWireframe
         {
             set
             {
-                if(SetValue(ref showWireframe, value))
+                if (SetValue(ref showWireframe, value))
                 {
-                    foreach(var model in ModelGeometry)
+                    foreach (var model in ModelGeometry)
                     {
                         (model as MeshGeometryModel3D).RenderWireframe = value;
                     }
@@ -44,30 +47,34 @@ namespace OrderIndependentTransparentRendering
 
         public ICommand ResetCameraCommand
         {
-            set;get;
+            set; get;
         }
-
 
         public MainViewModel()
         {
             this.ModelGeometry = new ObservableElement3DCollection();
             EffectsManager = new DefaultEffectsManager();
-            Camera = new OrthographicCamera() {
+            Camera = new OrthographicCamera()
+            {
                 LookDirection = new System.Windows.Media.Media3D.Vector3D(0, -50, -50),
                 Position = new System.Windows.Media.Media3D.Point3D(0, 50, 50),
-                FarPlaneDistance = 1000, NearPlaneDistance = 0.1, Width = 100 };
+                FarPlaneDistance = 1000,
+                NearPlaneDistance = 0.1,
+                Width = 100
+            };
             ResetCameraCommand = new RelayCommand((o) => { Camera.Reset(); });
             Load3ds("NITRO_ENGINE.3ds");
             BuildGrid();
+            BuildPlanes();
         }
 
         private void BuildGrid()
         {
             var builder = new LineBuilder();
             int zOff = -45;
-            for(int i =0; i< 10; ++i)
+            for (int i = 0; i < 10; ++i)
             {
-                for(int j =0; j < 10; ++j)
+                for (int j = 0; j < 10; ++j)
                 {
                     builder.AddLine(new SharpDX.Vector3(-i * 5, 0, j * 5), new SharpDX.Vector3(i * 5, 0, j * 5));
                     builder.AddLine(new SharpDX.Vector3(-i * 5, 0, -j * 5), new SharpDX.Vector3(i * 5, 0, -j * 5));
@@ -82,14 +89,60 @@ namespace OrderIndependentTransparentRendering
             GridTransform = new Media3D.TranslateTransform3D(new Media3D.Vector3D(0, -10, 0));
         }
 
+        private void BuildPlanes()
+        {
+            PlaneGeometry = new ObservableElement3DCollection();
+            var builder = new MeshBuilder(true);
+            builder.AddBox(new SharpDX.Vector3(0, 0, 0), 15, 15, 0.5);
+            var mesh = builder.ToMesh();
+
+            var material = new PhongMaterial();
+            material.DiffuseColor = new SharpDX.Color4(1, 0, 0, 0.5f);
+
+            var model = new MeshGeometryModel3D()
+            {
+                Geometry = mesh,
+                Material = material,
+                Transform = new Media3D.TranslateTransform3D(-15, 0, 0),
+                IsTransparent = true,
+                CullMode = SharpDX.Direct3D11.CullMode.Back
+            };
+            PlaneGeometry.Add(model);
+
+            material = new PhongMaterial();
+            material.DiffuseColor = new SharpDX.Color4(0, 1, 0, 0.5f);
+
+            model = new MeshGeometryModel3D()
+            {
+                Geometry = mesh,
+                Material = material,
+                Transform = new Media3D.TranslateTransform3D(-20, 5, -4),
+                IsTransparent = true,
+                CullMode = SharpDX.Direct3D11.CullMode.Back
+            };
+            PlaneGeometry.Add(model);
+
+            material = new PhongMaterial();
+            material.DiffuseColor = new SharpDX.Color4(0, 0, 1, 0.5f);
+
+            model = new MeshGeometryModel3D()
+            {
+                Geometry = mesh,
+                Material = material,
+                Transform = new Media3D.TranslateTransform3D(-25, 10, -8),
+                IsTransparent = true,
+                CullMode = SharpDX.Direct3D11.CullMode.Back
+            };
+            PlaneGeometry.Add(model);
+        }
 
         public void Load3ds(string path)
         {
             var reader = new StudioReader();
             var objCol = reader.Read(path);
             AttachModelList(objCol);
-
         }
+
         public void LoadObj(string path)
         {
             var reader = new ObjReader();
@@ -103,27 +156,29 @@ namespace OrderIndependentTransparentRendering
             var objCol = reader.Read(path);
             AttachModelList(objCol);
         }
+
         public void AttachModelList(List<Object3D> objs)
         {
             this.ModelGeometry = new ObservableElement3DCollection();
             Random rnd = new Random();
-            
+
             foreach (var ob in objs)
             {
                 var s = new MeshGeometryModel3D
                 {
                     Geometry = ob.Geometry,
                     Material = ob.Material,
-                    DepthBias = -100, SlopeScaledDepthBias = 0,
+                    DepthBias = -100,
+                    SlopeScaledDepthBias = 0,
                     CullMode = SharpDX.Direct3D11.CullMode.Back
                 };
-                if(ob.Material is PhongMaterial p)
+                if (ob.Material is PhongMaterial p)
                 {
                     var diffuse = p.DiffuseColor;
                     diffuse.Red = (float)rnd.NextDouble();
                     diffuse.Green = (float)rnd.NextDouble();
                     diffuse.Blue = (float)rnd.NextDouble();
-                    diffuse.Alpha = (float)(Math.Min(0.8, Math.Max(0.2, rnd.NextDouble())));
+                    diffuse.Alpha = 0.6f;//(float)(Math.Min(0.8, Math.Max(0.2, rnd.NextDouble())));
                     p.DiffuseColor = diffuse;
                     if (p.DiffuseColor.Alpha < 0.9)
                     {
@@ -135,10 +190,8 @@ namespace OrderIndependentTransparentRendering
                     s.Instances = ob.Transform;
                 }
                 this.ModelGeometry.Add(s);
-
             }
             this.OnPropertyChanged("ModelGeometry");
         }
     }
-
 }
