15c87184f8616a03c9a957ab8dec71d9d25d9365
holance <lunci.app@gmail.com>
2018-04-24 13:20:01 -0700

    Merge OIT calculation into common function

--- Source/HelixToolkit.SharpDX.ShaderBuilder/PS/psBillboardTextOIT.hlsl
+++ Source/HelixToolkit.SharpDX.ShaderBuilder/PS/psBillboardTextOIT.hlsl
@@ -4,22 +4,12 @@
 #include"..\Common\DataStructs.hlsl"
 #include"..\Common\Common.hlsl"
 #include"psBillboardText.hlsl"
+#include"psCommon.hlsl"
 
 PSOITOutput billboardTextOIT(PSInputBT input)
 {
-    PSOITOutput output = (PSOITOutput) 0;
     float4 color = main(input);
-    // Insert your favorite weighting function here. The color-based factor
-    // avoids color pollution from the edges of wispy clouds. The z-based
-    // factor gives precedence to nearer surfaces.
-    float weight = color.a * clamp(0.03 / (1e-5 + pow((input.p.z / input.p.w), 4.0)), 1e-2, 3e3);
-    // Blend Func: GL_ONE, GL_ONE
-    // Switch to premultiplied alpha and weight
-    output.color = float4(color.rgb * color.a, color.a) * weight;
- 
-    // Blend Func: GL_ZERO, GL_ONE_MINUS_SRC_ALPHA
-    output.alpha.a = color.a;
-    return output;
+    return calculateOIT(color, input.p);
 }
 
 #endif
