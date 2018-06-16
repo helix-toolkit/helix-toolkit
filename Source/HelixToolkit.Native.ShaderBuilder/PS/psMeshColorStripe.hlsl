6dad70890de2310fdd3f9feb9cb86fd7ba483cad
Lunci <lunci.app@gmail.com>
2018-06-15 23:30:02 -0700

    Improve color stripe pixel shader

--- Source/HelixToolkit.Native.ShaderBuilder/PS/psMeshColorStripe.hlsl
+++ Source/HelixToolkit.Native.ShaderBuilder/PS/psMeshColorStripe.hlsl
@@ -21,8 +21,8 @@ float4 main(PSInput input) : SV_Target
     }
     
     c *= input.cDiffuse;
-    float3 dir = normalize(vEyePos - input.wp.xyz);
-    float f = clamp(dot(dir, normalize(input.n)), 0.5f, 1);
-    c.rgb *= f;
+    float3 dir = normalize(input.vEye.xyz);
+    float f = 0.8 + dot(dir, normalize(input.n)) * 0.2;
+    c.rgb *= clamp(f, 0.8, 1);
     return saturate(c);
 }
