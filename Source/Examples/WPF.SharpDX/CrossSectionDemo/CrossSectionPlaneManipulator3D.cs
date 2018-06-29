1e19e8c8ba146437f72d393714cdfd42b4d0f840
holance <lunci.app@gmail.com>
2018-06-29 15:55:59 -0700

    Fix name space

--- Source/Examples/WPF.SharpDX/CrossSectionDemo/CrossSectionPlaneManipulator3D.cs
+++ Source/Examples/WPF.SharpDX/CrossSectionDemo/CrossSectionPlaneManipulator3D.cs
@@ -6,6 +6,8 @@ using MatrixTransform3D = System.Windows.Media.Media3D.MatrixTransform3D;
 using Matrix = System.Numerics.Matrix4x4;
 using System;
 using HelixToolkit.Mathematics;
+using HelixToolkit.Wpf.SharpDX.Model.Scene;
+using System.Collections.Generic;
 
 namespace CrossSectionDemo
 {
