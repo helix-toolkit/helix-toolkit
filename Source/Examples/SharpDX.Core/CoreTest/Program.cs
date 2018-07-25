b39dee4e6d8baacb7c3278de82483c282138d28a
Lunci <lunci.app@gmail.com>
2018-07-24 22:01:38 -0700

    Add nv optimus enabler for core tester

--- Source/Examples/SharpDX.Core/CoreTest/Program.cs
+++ Source/Examples/SharpDX.Core/CoreTest/Program.cs
@@ -15,11 +15,37 @@ namespace CoreTest
         [STAThread]
         static void Main()
         {
+            NVOptimusEnabler nvEnabler = new NVOptimusEnabler();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             RenderForm form = new RenderForm();
             CoreTestApp app = new CoreTestApp(form);
             Application.Run(form);
         }
+    }
+
+    public sealed class NVOptimusEnabler
+    {
+        static NVOptimusEnabler()
+        {
+            try
+            {
+
+                if (Environment.Is64BitProcess)
+                    NativeMethods.LoadNvApi64();
+                else
+                    NativeMethods.LoadNvApi32();
+            }
+            catch { } // will always fail since 'fake' entry point doesn't exists
+        }
+    };
+
+    internal static class NativeMethods
+    {
+        [System.Runtime.InteropServices.DllImport("nvapi64.dll", EntryPoint = "fake")]
+        internal static extern int LoadNvApi64();
+
+        [System.Runtime.InteropServices.DllImport("nvapi.dll", EntryPoint = "fake")]
+        internal static extern int LoadNvApi32();
     }
 }
