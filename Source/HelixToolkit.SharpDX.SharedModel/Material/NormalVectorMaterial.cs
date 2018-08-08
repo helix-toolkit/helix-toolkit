11aa1e5a5793686c22526f681ca0e11fdefcc026
Lunci <lunci.app@gmail.com>
2018-08-08 00:09:32 -0700

    Implement freezable for material. Only for WPF.SharpDX

--- Source/HelixToolkit.SharpDX.SharedModel/Material/NormalVectorMaterial.cs
+++ Source/HelixToolkit.SharpDX.SharedModel/Material/NormalVectorMaterial.cs
@@ -17,5 +17,15 @@ namespace HelixToolkit.Wpf.SharpDX
         {
             return NormalVectorMaterialCore.Core;
         }
+
+#if !NETFX_CORE
+        protected override Freezable CreateInstanceCore()
+        {
+            return new NormalVectorMaterial()
+            {
+                Name = Name
+            };
+        }
+#endif
     }
 }
