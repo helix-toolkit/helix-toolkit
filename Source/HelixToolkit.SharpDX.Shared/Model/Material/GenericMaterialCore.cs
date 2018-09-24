18ffb6732431a2fe4fe4a148f6fe357ff9c1b3fd
Lunci <lunci.app@gmail.com>
2018-09-23 19:51:42 -0700

    Add Generic Material for future use

---
+++ Source/HelixToolkit.SharpDX.Shared/Model/Material/GenericMaterialCore.cs
@@ -0,0 +1,246 @@
+﻿/*
+The MIT License (MIT)
+Copyright (c) 2018 Helix Toolkit contributors
+*/
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.IO;
+using global::SharpDX;
+using SharpDX.Direct3D11;
+#if !NETFX_CORE
+namespace HelixToolkit.Wpf.SharpDX.Model
+#else
+namespace HelixToolkit.UWP.Model
+#endif
+{
+    using Shaders;
+    
+
+    [DataContract]
+    public abstract class GenericMaterialCore : MaterialCore
+    {
+        internal sealed class UpdateEvent
+        {
+            public readonly GenericMaterialVariable.ResourceType Type;
+            public readonly string Name;
+            public UpdateEvent(GenericMaterialVariable.ResourceType type, string name)
+            {
+                Type = type;
+                Name = name;
+            }
+        }
+
+        internal event EventHandler<UpdateEvent> UpdatingResource;
+        [DataMember]
+        public Dictionary<string, Stream> TextureDict { get; } = new Dictionary<string, Stream>();
+        [DataMember]
+        public Dictionary<string, SamplerStateDescription> SamplerDict { get; } = new Dictionary<string, SamplerStateDescription>();
+        [DataMember]
+        public Dictionary<string, float> FloatDict { get; } = new Dictionary<string, float>();
+        [DataMember]
+        public Dictionary<string, bool> BoolDict { get; } = new Dictionary<string, bool>();
+        [DataMember]
+        public Dictionary<string, Vector2> Vector2Dict { get; } = new Dictionary<string, Vector2>();
+        [DataMember]
+        public Dictionary<string, Vector3> Vector3Dict { get; } = new Dictionary<string, Vector3>();
+        [DataMember]
+        public Dictionary<string, Vector4> Vector4Dict { get; } = new Dictionary<string, Vector4>();
+        [DataMember]
+        public Dictionary<string, Matrix> MatrixDict { get; } = new Dictionary<string, Matrix>();
+        [DataMember]
+        public string MaterialPassName { set; get; } = DefaultPassNames.Default;
+        [DataMember]
+        public string ShadowPassName { set; get; } = DefaultPassNames.ShadowPass;
+        [DataMember]
+        public string WireframePassName { set; get; } = DefaultPassNames.Wireframe;
+
+        public ConstantBufferDescription ConstantBufferDesc { set; get; } = MaterialVariable.DefaultMeshConstantBufferDesc;
+
+        public GenericMaterialCore(
+            string materialShaderPassName,
+            string shadowShaderPassName,
+            string wireframePassName)
+        {
+            this.MaterialPassName = materialShaderPassName;
+            this.ShadowPassName = shadowShaderPassName;
+            this.WireframePassName = wireframePassName;
+        }
+
+        public GenericMaterialCore()
+        {
+
+        }
+
+        public void SetTexture(string name, Stream texture)
+        {
+            if (TextureDict.ContainsKey(name))
+            {
+                TextureDict[name] = texture;
+            }
+            else
+            {
+                TextureDict.Add(name, texture);
+            }
+            UpdatingResource?.Invoke(this, new UpdateEvent(GenericMaterialVariable.ResourceType.Texture, name));
+        }
+
+
+        public void SetSampler(string name, SamplerStateDescription samplerDesc)
+        {
+            if (SamplerDict.ContainsKey(name))
+            {
+                SamplerDict[name] = samplerDesc;
+            }
+            else
+            {
+                SamplerDict.Add(name, samplerDesc);
+            }
+            UpdatingResource?.Invoke(this, new UpdateEvent(GenericMaterialVariable.ResourceType.Sampler, name));
+        }
+
+        public Stream GetTexture(string name)
+        {
+            if(TextureDict.TryGetValue(name, out var texture))
+            {
+                return texture;
+            }
+            else
+            {
+                return null;
+            }
+            
+        }
+
+        public SamplerStateDescription GetSampler(string name)
+        {
+            if(SamplerDict.TryGetValue(name, out var samplerDesc))
+            {
+                return samplerDesc;
+            }
+            else
+            {
+                return new SamplerStateDescription();
+            }         
+        }
+
+
+        public void SetProperty(string name, int value)
+        {
+            if (FloatDict.ContainsKey(name))
+            {
+                FloatDict[name] = value;
+            }
+            else
+            {
+                FloatDict.Add(name, value);
+            }
+            UpdatingResource?.Invoke(this, new UpdateEvent(GenericMaterialVariable.ResourceType.Float, name));
+        }
+
+        public void SetProperty(string name, float value)
+        {
+            if (FloatDict.ContainsKey(name))
+            {
+                FloatDict[name] = value;
+            }
+            else
+            {
+                FloatDict.Add(name, value);
+            }
+            UpdatingResource?.Invoke(this, new UpdateEvent(GenericMaterialVariable.ResourceType.Float, name));
+        }
+
+        public void SetProperty(string name, bool value)
+        {
+            if (FloatDict.ContainsKey(name))
+            {
+                FloatDict[name] = value ? 1 : 0;
+            }
+            else
+            {
+                FloatDict.Add(name, value ? 1 : 0);
+            }
+            UpdatingResource?.Invoke(this, new UpdateEvent(GenericMaterialVariable.ResourceType.Float, name));
+        }
+
+        public void SetProperty(string name, Vector2 value)
+        {
+            if (Vector2Dict.ContainsKey(name))
+            {
+                Vector2Dict[name] = value;
+            }
+            else
+            {
+                Vector2Dict.Add(name, value);
+            }
+            UpdatingResource?.Invoke(this, new UpdateEvent(GenericMaterialVariable.ResourceType.Vector2, name));
+        }
+
+        public void SetProperty(string name, Vector3 value)
+        {
+            if (Vector3Dict.ContainsKey(name))
+            {
+                Vector3Dict[name] = value;
+            }
+            else
+            {
+                Vector3Dict.Add(name, value);
+            }
+            UpdatingResource?.Invoke(this, new UpdateEvent(GenericMaterialVariable.ResourceType.Vector3, name));
+        }
+
+        public void SetProperty(string name, Vector4 value)
+        {
+            if (Vector4Dict.ContainsKey(name))
+            {
+                Vector4Dict[name] = value;
+            }
+            else
+            {
+                Vector4Dict.Add(name, value);
+            }
+            UpdatingResource?.Invoke(this, new UpdateEvent(GenericMaterialVariable.ResourceType.Vector4, name));
+        }
+
+        public void SetProperty(string name, Matrix value)
+        {
+            if (MatrixDict.ContainsKey(name))
+            {
+                MatrixDict[name] = value;
+            }
+            else
+            {
+                MatrixDict.Add(name, value);
+            }
+            UpdatingResource?.Invoke(this, new UpdateEvent(GenericMaterialVariable.ResourceType.Matrix, name));
+        }
+    }
+
+    public sealed class GenericMeshMaterialCore : GenericMaterialCore
+    {
+        public override MaterialVariable CreateMaterialVariables(IEffectsManager manager, IRenderTechnique technique)
+        {
+            return new GenericMeshMaterialVariable(manager, technique, this, MaterialVariable.DefaultMeshConstantBufferDesc,
+                MaterialPassName, ShadowPassName, WireframePassName);
+        }
+    }
+
+    public sealed class GenericLineMaterialCore : GenericMaterialCore
+    {
+        public override MaterialVariable CreateMaterialVariables(IEffectsManager manager, IRenderTechnique technique)
+        {
+            return new GenericMeshMaterialVariable(manager, technique, this, MaterialVariable.DefaultPointLineConstantBufferDesc,
+                MaterialPassName, ShadowPassName, "");
+        }
+    }
+
+    public sealed class GenericPointMaterialCore : GenericMaterialCore
+    {
+        public override MaterialVariable CreateMaterialVariables(IEffectsManager manager, IRenderTechnique technique)
+        {
+            return new GenericPointMaterialVariable(manager, technique, this, MaterialVariable.DefaultPointLineConstantBufferDesc,
+                MaterialPassName, ShadowPassName);
+        }
+    }
+}
---
+++ Source/HelixToolkit.SharpDX.Shared/Model/Material/Variables/GenericMaterialVariable.cs
@@ -0,0 +1,230 @@
+﻿/*
+The MIT License (MIT)
+Copyright (c) 2018 Helix Toolkit contributors
+*/
+#if !NETFX_CORE
+namespace HelixToolkit.Wpf.SharpDX.Model
+#else
+namespace HelixToolkit.UWP.Model
+#endif
+{
+    using Render;
+    using Shaders;
+    using System.Collections.Generic;
+    using System.IO;
+    using Utilities;
+
+    public abstract class GenericMaterialVariable : MaterialVariable
+    {
+        public enum ResourceType
+        {
+            Texture, Sampler, Float, Vector2, Vector3, Vector4, Matrix
+        }
+        private readonly ShaderPass materialPass, shadowPass, wireframePass;
+
+
+        private readonly KeyValuePair<int, ShaderResourceViewProxy>[] shaderResources;
+        private readonly KeyValuePair<int, SamplerStateProxy>[] samplerResources;
+
+        private readonly Dictionary<string, int> resourceIdxDict = new Dictionary<string, int>();
+        private readonly Dictionary<string, int> samplerIdxDict = new Dictionary<string, int>();
+        private readonly GenericMaterialCore core;
+
+        public GenericMaterialVariable(IEffectsManager manager, IRenderTechnique technique,
+            GenericMaterialCore materialCore, ConstantBufferDescription constantBufferDescription,
+            string materialShaderPassName = DefaultPassNames.Default,
+            string shadowShaderPassName = DefaultPassNames.ShadowPass,
+            string wireframePassName = DefaultPassNames.Wireframe)
+            : base(manager, technique, constantBufferDescription, materialCore)
+        {
+            core = materialCore;
+            materialPass = technique[materialShaderPassName];
+            shadowPass = technique[shadowShaderPassName];
+            wireframePass = technique[wireframePassName];
+            shaderResources = new KeyValuePair<int, ShaderResourceViewProxy>[materialPass.PixelShader.ShaderResourceViewMapping.Count];
+
+            for(int i=0; i < materialPass.PixelShader.ShaderResourceViewMapping.Count; ++i)
+            {
+                var mapping = materialPass.PixelShader.ShaderResourceViewMapping.Mappings[i];
+                resourceIdxDict.Add(mapping.Value.Description.Name, i);
+                shaderResources[i] = new KeyValuePair<int, ShaderResourceViewProxy>(mapping.Key, null);
+            }
+
+            samplerResources = new KeyValuePair<int, SamplerStateProxy>[materialPass.PixelShader.SamplerMapping.Count];
+
+            for(int i=0; i < materialPass.PixelShader.SamplerMapping.Count; ++i)
+            {
+                var mapping = materialPass.PixelShader.SamplerMapping.Mappings[i];
+                samplerIdxDict.Add(mapping.Value.Name, i);
+                samplerResources[i] = new KeyValuePair<int, SamplerStateProxy>(mapping.Key, null);
+            }
+
+            foreach(var texture in materialCore.TextureDict)
+            {
+                SetTexture(texture.Key, texture.Value);
+            }
+
+            foreach(var sampler in materialCore.SamplerDict)
+            {
+                SetSampler(sampler.Key, sampler.Value);
+            }
+
+            foreach(var prop in materialCore.FloatDict)
+            {
+                WriteValue(prop.Key, prop.Value);
+            }
+            foreach (var prop in materialCore.Vector2Dict)
+            {
+                WriteValue(prop.Key, prop.Value);
+            }
+            foreach (var prop in materialCore.Vector3Dict)
+            {
+                WriteValue(prop.Key, prop.Value);
+            }
+            foreach (var prop in materialCore.Vector4Dict)
+            {
+                WriteValue(prop.Key, prop.Value);
+            }
+            foreach (var prop in materialCore.MatrixDict)
+            {
+                WriteValue(prop.Key, prop.Value);
+            }
+            materialCore.UpdatingResource += MaterialCore_UpdatingResource;
+        }
+
+        private void MaterialCore_UpdatingResource(object sender, GenericMaterialCore.UpdateEvent e)
+        {
+            switch (e.Type)
+            {
+                case ResourceType.Sampler:
+                    SetSampler(e.Name, core.GetSampler(e.Name));
+                    break;
+                case ResourceType.Texture:
+                    SetTexture(e.Name, core.GetTexture(e.Name));
+                    break;
+                case ResourceType.Float:
+                    WriteValue(e.Name, core.FloatDict[e.Name]);
+                    break;
+                case ResourceType.Vector2:
+                    WriteValue(e.Name, core.Vector2Dict[e.Name]);
+                    break;
+                case ResourceType.Vector3:
+                    WriteValue(e.Name, core.Vector3Dict[e.Name]);
+                    break;
+                case ResourceType.Vector4:
+                    WriteValue(e.Name, core.Vector4Dict[e.Name]);
+                    break;
+                case ResourceType.Matrix:
+                    WriteValue(e.Name, core.MatrixDict[e.Name]);
+                    break;
+            }
+        }
+
+        public bool SetTexture(string name, Stream texture)
+        {
+            if(resourceIdxDict.TryGetValue(name, out var idx))
+            {
+                var exist = shaderResources[idx].Value;
+                RemoveAndDispose(ref exist);
+                if (texture == null)
+                {
+                    shaderResources[idx] = new KeyValuePair<int, ShaderResourceViewProxy>(shaderResources[idx].Key, null);
+                }
+                else
+                {
+                    var res = Collect(EffectsManager.MaterialTextureManager.Register(texture));
+                    shaderResources[idx] = new KeyValuePair<int, ShaderResourceViewProxy>(shaderResources[idx].Key, res);
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool SetSampler(string name, global::SharpDX.Direct3D11.SamplerStateDescription sampler)
+        {
+            if (resourceIdxDict.TryGetValue(name, out var idx))
+            {
+                var exist = samplerResources[idx].Value;
+                RemoveAndDispose(ref exist);
+                var res = Collect(EffectsManager.StateManager.Register(sampler));
+                samplerResources[idx] = new KeyValuePair<int, SamplerStateProxy>(samplerResources[idx].Key, res);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override bool BindMaterialResources(RenderContext context, DeviceContextProxy deviceContext, ShaderPass shaderPass)
+        {
+            foreach(var res in shaderResources)
+            {
+                deviceContext.SetShaderResource(PixelShader.Type, res.Key, res.Value);
+            }
+            return true;
+        }
+
+        public override ShaderPass GetPass(RenderType renderType, RenderContext context)
+        {
+            return materialPass;
+        }
+
+        public override ShaderPass GetShadowPass(RenderType renderType, RenderContext context)
+        {
+            return shadowPass;
+        }
+
+        public override ShaderPass GetWireframePass(RenderType renderType, RenderContext context)
+        {
+            return wireframePass;
+        }
+
+        protected override void OnDispose(bool disposeManagedResources)
+        {
+            if (disposeManagedResources)
+            {
+                core.UpdatingResource -= MaterialCore_UpdatingResource;
+            }
+            base.OnDispose(disposeManagedResources);
+        }
+    }
+
+    public sealed class GenericMeshMaterialVariable : GenericMaterialVariable
+    {
+        public GenericMeshMaterialVariable(IEffectsManager manager, IRenderTechnique technique,
+            GenericMaterialCore materialCore, ConstantBufferDescription constantBufferDescription,
+            string materialShaderPassName = DefaultPassNames.Default,
+            string shadowShaderPassName = DefaultPassNames.ShadowPass,
+            string wireframePassName = DefaultPassNames.Wireframe)
+            :base(manager, technique, materialCore, constantBufferDescription, materialShaderPassName, shadowShaderPassName, wireframePassName)
+        {
+
+        }
+
+        public override void Draw(DeviceContextProxy deviceContext, IAttachableBufferModel bufferModel, int instanceCount)
+        {
+            DrawIndexed(deviceContext, bufferModel.IndexBuffer.ElementCount, instanceCount);
+        }
+    }
+
+    public sealed class GenericPointMaterialVariable : GenericMaterialVariable
+    {
+        public GenericPointMaterialVariable(IEffectsManager manager, IRenderTechnique technique,
+            GenericMaterialCore materialCore, ConstantBufferDescription constantBufferDescription,
+            string materialShaderPassName = DefaultPassNames.Default,
+            string shadowShaderPassName = DefaultPassNames.ShadowPass)
+            : base(manager, technique, materialCore, constantBufferDescription, materialShaderPassName, shadowShaderPassName, "")
+        {
+
+        }
+
+        public override void Draw(DeviceContextProxy deviceContext, IAttachableBufferModel bufferModel, int instanceCount)
+        {
+            DrawPoints(deviceContext, bufferModel.VertexBuffer[0].ElementCount, instanceCount);
+        }
+    }
+}
