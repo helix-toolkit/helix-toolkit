7ca561e6834bebb7f52e5076381879d5eab27702
Lunci <lunci.app@gmail.com>
2019-01-05 12:41:45 -0800

    Update normal cal under flat mode. Add RenderFlat option in FileLoadDemo

--- Source/HelixToolkit.Native.ShaderBuilder/PS/psMeshPBR.hlsl
+++ Source/HelixToolkit.Native.ShaderBuilder/PS/psMeshPBR.hlsl
@@ -11,37 +11,30 @@
 
 float3 calcNormal(PSInput input)
 {
-    if (bRenderFlat)
-    {       
-        return normalize(cross(ddy(input.wp.xyz), ddx(input.wp.xyz)));
-    }
-    else
+    float3 normal = bRenderFlat ? normalize(cross(ddy(input.wp.xyz), ddx(input.wp.xyz))) : normalize(input.n);
+    if (bHasNormalMap)
     {
-        float3 normal = normalize(input.n);
-        if (bHasNormalMap)
+        if (bAutoTengent)
         {
-            if (bAutoTengent)
-            {
-                float3 localNormal = BiasX2(texNormalMap.Sample(samplerSurface, input.t).xyz);
-                normal = PeturbNormal(localNormal, input.wp.xyz, normal, input.t);
-            }
-            else
-            {
-		    // Normalize the per-pixel interpolated tangent-space
-                float3 tangent = normalize(input.t1);
-                float3 biTangent = normalize(input.t2);
-
-		    // Sample the texel in the bump map.
-                float3 bumpMap = texNormalMap.Sample(samplerSurface, input.t);
-		    // Expand the range of the normal value from (0, +1) to (-1, +1).
-                bumpMap = mad(2.0f, bumpMap, -1.0f);
-		    // Calculate the normal from the data in the bump map.
-                normal += mad(bumpMap.x, tangent, bumpMap.y * biTangent);
-                normal = normalize(normal);
-            }
+            float3 localNormal = BiasX2(texNormalMap.Sample(samplerSurface, input.t).xyz);
+            normal = PeturbNormal(localNormal, input.wp.xyz, normal, input.t);
         }
-        return normal;
+        else
+        {
+		// Normalize the per-pixel interpolated tangent-space
+            float3 tangent = normalize(input.t1);
+            float3 biTangent = normalize(input.t2);
+
+		// Sample the texel in the bump map.
+            float3 bumpMap = texNormalMap.Sample(samplerSurface, input.t);
+		// Expand the range of the normal value from (0, +1) to (-1, +1).
+            bumpMap = mad(2.0f, bumpMap, -1.0f);
+		// Calculate the normal from the data in the bump map.
+            normal += mad(bumpMap.x, tangent, bumpMap.y * biTangent);
+            normal = normalize(normal);
+        }      
     }
+    return normal;
 }
 
 float3 LightSurface(in float4 wp,
