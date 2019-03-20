bf78f68e826402ab799959fd431541891019bb88
Lunci <lunci.app@gmail.com>
2019-03-20 00:01:58 -0700

    Add continuous render node/model to let render host render in each frame. Render host default behavior is lazy rendering. Use this to achieve busy rendering.

---
+++ Source/HelixToolkit.SharpDX.SharedModel/Element3D/ContinuousRender3D.cs
@@ -0,0 +1,25 @@
+﻿#if NETFX_CORE
+using Windows.UI.Xaml;
+namespace HelixToolkit.UWP
+#else
+using System.Windows;
+namespace HelixToolkit.Wpf.SharpDX
+#endif
+{
+    using Model.Scene;
+    /// <summary>
+    /// Use this model to keep update rendering in each frame.
+    /// <para>Default behavior for render host is lazy rendering. 
+    /// Only property changes trigger a render inside render loop. 
+    /// However, sometimes user want to keep updating rendering each frame while doing shader animation using TimeStamp.
+    /// Use this model to invalidate rendering in each frame and keep render host busy.
+    /// </para>
+    /// </summary>
+    public sealed class ContinuousRender3D : Element3D
+    {
+        protected override SceneNode OnCreateSceneNode()
+        {
+            return new ContinuousRenderNode();
+        }
+    }
+}
