65ce571e5ae12cbeaa8227d5d08ce475d7253a07
Christof Konstantinopoulos <ckonstantinopoulos@epmap-system.com>
2020-05-14 16:45:29 +0200

    Bug BillboardText3D

---
+++ Source/Examples/WPF.SharpDX/ExampleBrowser/Workitems/BugReportBillboard/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Helix Toolkit">
+//   Copyright (c) 2014 Helix Toolkit contributors
+// </copyright>
+// <summary>
+//   Interaction logic for MainWindow.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Windows;
+
+namespace ExampleBrowser.Workitems.BugReportBillboard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    [Example("Bugfix Billboard Text", "Viewport3DX BillboardText")]
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
