43e9b948779dab9f9d3f0d7faff799c1436f3b6b
Christof Konstantinopoulos <ckonstantinopoulos@epmap-system.com>
2020-11-28 01:25:32 +0100

    Example "RectangleNormalVector" added for issue #1461.

---
+++ Source/Examples/WPF/ExampleBrowser/Examples/RectangleNormalVector/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Helix Toolkit">
+//   Copyright (c) 2014 Helix Toolkit contributors
+// </copyright>
+// <summary>
+//   Interaction logic for MainWindow.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Windows;
+using ExampleBrowser;
+
+namespace RectangleNormalVector
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    [Example(null, "Test of rectangle3D orientations defined by normal vectors. Just use the sliders to manipulate the normal vector.")]
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Source/Examples/WPF/ExampleBrowser/Examples/RectangleNormalVector/ToVector3DConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media.Media3D;
+
+namespace RectangleNormalVector
+{
+    public class ToVector3DConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, 
+            object parameter, System.Globalization.CultureInfo culture)
+        {
+            var result = new Vector3D {X = (double) values[0], Y = (double) values[1], Z = (double) values[2]};
+            return result;
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, 
+            object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException("Cannot convert back");
+        }
+    }
+}
