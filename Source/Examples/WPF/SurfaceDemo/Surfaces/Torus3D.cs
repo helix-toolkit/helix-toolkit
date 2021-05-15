8eb6d48719130207a09a20444d622eafa7d89cae
Gustav Schubert <gschub@googlemail.com>
2021-05-15 12:13:28 +0200

    Dealing with glitches # 1586

--- Source/Examples/WPF/SurfaceDemo/Surfaces/Torus3D.cs
+++ Source/Examples/WPF/SurfaceDemo/Surfaces/Torus3D.cs
@@ -29,13 +29,13 @@ namespace SurfaceDemo
             double theta1 = u * 2 * Math.PI;
             double phi1 = v * 2 * Math.PI;
 
-            double r   = R1 + AMP * Math.Sin(FREQ * phi1);
-			double ang = phi1 + pi*2 * Math.Sin(TURNS * theta1);
+            double r = R1 + AMP * Math.Sin(FREQ * phi1);
+            double ang = phi1 + pi * 2 * Math.Sin(TURNS * theta1);
 
-			double tx = r * Math.Cos(ang) + R0;
-			double z = r * Math.Sin(ang);
-			double x = tx * Math.Cos(theta1);
-			double y = tx * Math.Sin(theta1);
+            double tx = (r * Math.Cos(ang)) + R0;
+            double z = r * Math.Sin(ang);
+            double x = tx * Math.Cos(theta1);
+            double y = tx * Math.Sin(theta1);
 
             texCoord = new Point(color, 0);
             return new Point3D(x, y, z);
