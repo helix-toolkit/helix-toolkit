0a637ed5e9e6d83749e329ec551a96d219730d42
Lunci <lunci.app@gmail.com>
2021-05-23 17:00:03 -0700

    Add TopMostGroupNode and TopMostGroup3D to support top most rendering. #1572

---
+++ Source/HelixToolkit.SharpDX.Shared/Core/TopMostMeshRenderCore.cs
@@ -0,0 +1,55 @@
+﻿/*
+The MIT License (MIT)
+Copyright (c) 2021 Helix Toolkit contributors
+*/
+using SharpDX.Direct3D11;
+#if !NETFX_CORE
+namespace HelixToolkit.Wpf.SharpDX
+#else
+#if CORE
+namespace HelixToolkit.SharpDX.Core
+#else
+namespace HelixToolkit.UWP
+#endif
+#endif
+{
+    namespace Core
+    {
+        using Shaders;
+        using Render;
+        using Components;
+        /// <summary>
+        /// Clears the depth buffer and reset global transform.
+        /// </summary>
+        public class TopMostMeshRenderCore: RenderCore
+        {
+            private readonly ConstantBufferComponent globalTransformCB;
+            public TopMostMeshRenderCore() : base(RenderType.ScreenSpaced)
+            {
+                globalTransformCB = AddComponent(new ConstantBufferComponent(new ConstantBufferDescription(DefaultBufferNames.GlobalTransformCB, GlobalTransformStruct.SizeInBytes)));
+            }
+            public override void Render(RenderContext context, DeviceContextProxy deviceContext)
+            {
+                if (RenderType != RenderType.ScreenSpaced)
+                {
+                    return;
+                }
+                deviceContext.GetDepthStencilView(out DepthStencilView dsView);
+                if (dsView == null)
+                {
+                    return;
+                }
+
+                deviceContext.ClearDepthStencilView(dsView, DepthStencilClearFlags.Depth, 1f, 0);
+                dsView.Dispose();
+                var globalTrans = context.GlobalTransform;
+                globalTransformCB.Upload(deviceContext, ref globalTrans);
+            }
+
+            protected override bool OnAttach(IRenderTechnique technique)
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Source/HelixToolkit.SharpDX.Shared/Model/Scene/TopMostGroupNode.cs
@@ -0,0 +1,49 @@
+﻿/*
+The MIT License (MIT)
+Copyright (c) 2021 Helix Toolkit contributors
+*/
+using SharpDX;
+using System;
+using System.Collections.Generic;
+
+#if !NETFX_CORE
+namespace HelixToolkit.Wpf.SharpDX
+#else
+#if CORE
+namespace HelixToolkit.SharpDX.Core
+#else
+namespace HelixToolkit.UWP
+#endif
+#endif
+{
+    namespace Model.Scene
+    {
+        using Core;
+        using Cameras;
+        /// <summary>
+        /// Provides a way to render child elements always on top of other elements.
+        /// This is rendered at the same level of screen spaced group items.
+        /// Child items do not support post effects.
+        /// </summary>
+        public class TopMostGroupNode : GroupNode
+        {
+            private bool enableTopMost = false;
+            public bool EnableTopMost
+            {
+                set
+                {
+                    if (SetAffectsRender(ref enableTopMost, value))
+                    {
+                        RenderType = value ? RenderType.ScreenSpaced : RenderType.Opaque;
+                    }
+                }
+                get => enableTopMost;
+            }
+            protected override RenderCore OnCreateRenderCore()
+            {
+                var core = new TopMostMeshRenderCore();
+                return core;
+            }
+        }
+    }
+}
