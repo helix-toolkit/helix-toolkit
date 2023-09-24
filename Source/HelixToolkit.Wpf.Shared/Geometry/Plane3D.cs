abda1f5c0375b4b81b287d46a301ad2ee8a5b6c3
maunv <nguyenvanmau9296@gmail.com>
2023-09-24 19:00:11 +0700

    Update doc comment

--- Source/HelixToolkit.Wpf.Shared/Geometry/Plane3D.cs
+++ Source/HelixToolkit.Wpf.Shared/Geometry/Plane3D.cs
@@ -3,7 +3,7 @@
 //   Copyright (c) 2014 Helix Toolkit contributors
 // </copyright>
 // <summary>
-//   Represents a plane.
+//   Represents a plane in three-dimensional space.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -12,7 +12,7 @@ namespace HelixToolkit.Wpf
     using System.Windows.Media.Media3D;
 
     /// <summary>
-    /// Represents a plane.
+    /// Represents a plane in three-dimensional space.
     /// </summary>
     public class Plane3D
     {
