db76e43e2ea5b06486354c0b50c41dd66bd4cef8
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2023-11-24 19:55:18 +0100

    Add HelixToolkit.Wpf.TDxInput

---
+++ Source/HelixToolkit.Wpf.TDxInput/SpaceNavigatorZoomMode.cs
@@ -0,0 +1,17 @@
+﻿namespace HelixToolkit.Wpf.TDxInput;
+
+/// <summary>
+/// Zoom mode.
+/// </summary>
+public enum SpaceNavigatorZoomMode
+{
+    /// <summary>
+    /// In and out.
+    /// </summary>
+    InOut,
+
+    /// <summary>
+    /// Up and down.
+    /// </summary>
+    UpDown
+}
