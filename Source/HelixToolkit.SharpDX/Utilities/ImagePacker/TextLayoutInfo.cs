17f308122ccd97d79eb945e437e0bd6b23f200ae
Lunci <lunci.app@gmail.com>
2024-10-15 12:58:41 -0700

    Fix ImagePacker memory leak. (#2277)

--- Source/HelixToolkit.SharpDX/Utilities/ImagePacker/TextLayoutInfo.cs
+++ Source/HelixToolkit.SharpDX/Utilities/ImagePacker/TextLayoutInfo.cs
@@ -3,7 +3,7 @@ using SharpDX;
 
 namespace HelixToolkit.SharpDX.Utilities.ImagePacker;
 
-public readonly struct TextLayoutInfo
+public sealed class TextLayoutInfo : IDisposable
 {
     public readonly TextLayout TextLayout;
     public readonly Color4 Foreground;
@@ -16,4 +16,38 @@ public readonly struct TextLayoutInfo
         Background = background;
         Padding = padding;
     }
+
+    #region IDisposable Support
+    private bool disposedValue = false; // To detect redundant calls
+
+    void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+            // TODO: set large fields to null.
+            TextLayout.Dispose();
+            disposedValue = true;
+        }
+    }
+
+    // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+    // ~ImagePacker() {
+    //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+    //   Dispose(false);
+    // }
+
+    // This code added to correctly implement the disposable pattern.
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        Dispose(true);
+        // TODO: uncomment the following line if the finalizer is overridden above.
+        // GC.SuppressFinalize(this);
+    }
+    #endregion
 }
